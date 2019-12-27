e7f0f04a8fae4a55a9f7ed74e8a89b814935985a
BRANDON JUAREZ PONCE <MRT15021@MEXICO.TEM.MX>
2019-12-27 16:08:52 -0600

    Subiendo la mayoria de la información

---
+++ CSSsJSs/script.js
@@ -0,0 +1,12 @@
+
+function opacity(shID) {
+    document.getElementById(shID).style.opacity = 1.0;
+}
+
+function entrada(){
+    //transition: opacity 2s ease-in-out;
+    setTimeout(function(){opacity("seccion1");},100);
+    setTimeout(function(){opacity("seccion2");},800);
+    setTimeout(function(){opacity("seccion3");},1500);
+    setTimeout(function(){opacity("seccion4");},2200);
+}
