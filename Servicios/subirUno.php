7cc50db0f7c4790e63eea1062df74aef579fdf26
brjupo <brjupo@gmail.com>
2020-03-29 14:31:44 -0600

    psd20

--- Servicios/subirUno.php
+++ Servicios/subirUno.php
@@ -17,7 +17,7 @@ $resultadoSuper = mysqli_query($con, $sql);
 $arraySuper = mysqli_fetch_array($resultadoSuper);
 if ($arraySuper[0] != $password) {
     $response = array();
-    $response['response'] = $arraySuper[0];
+    $response['response'] = 'Error en la contraseña';
     echo json_encode($response);
 } else {
     if ($rowp) {
@@ -31,7 +31,15 @@ if ($arraySuper[0] != $password) {
         //Agregar a la base de datos
         $sql = "INSERT INTO usuario_prueba(mail, pswd, tokenA) VALUES ('$correo_e', '1234', '$rand')";
         mysqli_query($con, $sql);
-
+        //Enviar correo a usuario para que genere su contrasenia
+        //mail(correo,asunto,cuerpo);
+        $cuerpo="Hola! En la siguiente liga podrás cambiar tu contraseña. 
+                    https://kaanbal.net/Front/errorInfoPages/password.php?token=". 
+                    $rand . "&correo=" . $correo_e . 
+                    "   Recuerda, esta liga es de un solo uso e instransferible. 
+                    Si vuelves a olvidarla. Ingresa a https://kaanbal.net y 
+                    elige la opción olvidé mi contraseña.";
+        mail($correo_e, "Kaanbal - Contraseña", $cuerpo);
         //Si no existe, regresar true
         $response = array();
         $response['response'] = 'true';
