e442edabcc3730c132049fa383f3713b1f53ac0c
brjupo <brjupo@gmail.com>
2020-04-05 01:50:41 -0600

    vista preguntas disque lista

--- Front/preguntas/preguntaSprint.php
+++ Front/preguntas/preguntaSprint.php
@@ -12,35 +12,67 @@
 </head>
 
 <body>
+<?php
+  $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
+  //Traer todas las preguntas
+  $query = "SELECT * FROM pregunta LIMIT 10"; //WHERE TEMA = 'TEMA' AND SUBTEMA = 'SUBTEMA' AND LECCION = 'LECCION'";     
+  $result = mysqli_query($con, $query);
+  //contar Numero de elementos
+  //$query2 = "Select count(*) FROM pregunta WHERE id_leccion = 1"; // WHERE TEMA = 'TEMA' AND SUBTEMA = 'SUBTEMA' AND LECCION = 'LECCION'";
+  //$result2 = mysqli_query($con, $query2);
+  //$total = mysqli_fetch_row($result2);
+  $total = 10;
+  //Recorrer el arreglo
+  while ($row = mysqli_fetch_assoc($result)) {
+    $array[] = $row;
+    $arrayr[] = $row;
+  }
+  $respuestas = array('respuesta_correcta', 'respuesta2', 'respuesta3', 'respuesta4');
+  $respuestasr = array('respuesta_correcta', 'respuesta2', 'respuesta3', 'respuesta4');
+  $s=0;
+  for ($j = 0; $j < $total; $j++) {
+    $i = 0;
+    shuffle($respuestas);
+    foreach ($respuestas as $val) {
+      //print_r ($val);
+      $arrayr[$j][$respuestasr[$i]] = $array[$j][$val];
+      //print_r ($i);
+      $i = $i + 1;
+    }
+  }
+  //print_r($arrayr);
+  ?>
+  
   <?php
-  imprimirPreguntas();
-
+  imprimirPreguntas($arrayr, $array);
   ?>
 
   <?php
-  function imprimirPreguntas()
+  function imprimirPreguntas($arrayr, $array)
   {
     imprimirBarraProgresoCruz();
     imprimirContador();
-    imprimirPreguntasRespuestas();
+    imprimirPreguntasRespuestas($arrayr, $array);
     imprimirFooter();
   }
   ?>
 
   <?php
-  function imprimirPreguntasRespuestas()
+  function imprimirPreguntasRespuestas($arrayr, $array)
   {
     $p1 = "¿Cuál es la equivalencia correcta para la expresion
-    10^6?";
+  10^6?";
     $r1 = "100,000";
     $r2 = "10,000,000";
     $r3 = "1,000,000";
     $r4 = "10,000";
     $rc = "10,000,000";
     //Se imprime las siguientes preguntas INVISIBLES
-    for ($x = 1; $x <= 10; $x++) {
-      imprimirPregunta($x,$p1);
-      imprimirImagenRespuestas($x, $r1, $r2, $r3, $r4, $rc);
+    for ($x = 0; $x <= 9; $x++) {
+      imprimirPregunta($x + 1, $arrayr[$x]["pregunta"]);
+      imprimirImagenRespuestas($x + 1, $arrayr[$x]["respuesta_correcta"],
+       $arrayr[$x]["respuesta2"], $arrayr[$x]["respuesta3"],
+       $arrayr[$x]["respuesta4"], $array[$x]["respuesta_correcta"]);
     }
   }
   ?>
@@ -52,112 +84,112 @@
   function imprimirBarraProgresoCruz()
   {
     echo '
-      <div class="container">
-        <div class="row topMargin">
-          <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
-            <img src="../CSSsJSs/icons/clear.svg" id="cruzCerrar" class="cruz" />
-          </div>
-          <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
-            <div class="progress progressMargin">
-              <!-- class="active"-->
-              <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
-            </div>
+    <div class="container">
+      <div class="row topMargin">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+          <img src="../CSSsJSs/icons/clear.svg" id="cruzCerrar" class="cruz" />
+        </div>
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
+          <div class="progress progressMargin">
+            <!-- class="active"-->
+            <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
           </div>
         </div>
       </div>
-    ';
+    </div>
+  ';
   }
   function imprimirContador()
   {
     echo '
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-          <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
-            <p class="slide-bottom" id="previous">0m 10s</p>
-            <p class="slide-bottom" id="actual">0m 10s</p>
-            <p class="slide-bottom" id="later">0m 10s</p>
-            <p id="puntosBuenos"></p>
-          </div>
-          <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
+          <p class="slide-bottom" id="previous">0m 10s</p>
+          <p class="slide-bottom" id="actual">0m 10s</p>
+          <p class="slide-bottom" id="later">0m 10s</p>
+          <p id="puntosBuenos"></p>
         </div>
+        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
       </div>
-    ';
+    </div>
+  ';
   }
 
   function imprimirPregunta(int $preguntaNumero, $preguntaTexto)
   {
-    $preguntaNumero=100+$preguntaNumero;
+    $preguntaNumero = 100 + $preguntaNumero;
     echo '
-      <!--+++++++++++++++++++++++++++++++++++++++PREGUNTA++++++++++++++++++++++++++++++++++++++++++++-->
-      <div class="container" style="display:none" id="'.$preguntaNumero.'">
-        <div class="row">
-          <div class="hidden-xs hidden-sm col-md-1 col-lg-1 col-xl-1"></div>
-          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
-            <p id="preguntaNumero">'.$preguntaNumero.'</p>
-            <p class="formatoPreguntas">'
-            . $preguntaTexto .
-            '  
-            </p>
-          </div>
-          <div class="hidden-xs hidden-sm col-md-1 col-lg-1 col-xl-1"></div>
+    <!--+++++++++++++++++++++++++++++++++++++++PREGUNTA++++++++++++++++++++++++++++++++++++++++++++-->
+    <div class="container" style="display:none" id="' . $preguntaNumero . '">
+      <div class="row">
+        <div class="hidden-xs hidden-sm col-md-1 col-lg-1 col-xl-1"></div>
+        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
+          <p id="preguntaNumero">' . $preguntaNumero . '</p>
+          <p class="formatoPreguntas">'
+      . $preguntaTexto .
+      '  
+          </p>
         </div>
+        <div class="hidden-xs hidden-sm col-md-1 col-lg-1 col-xl-1"></div>
       </div>
-    ';
+    </div>
+  ';
   }
-  function imprimirImagenRespuestas(int $respuestas,$r1, $r2, $r3, $r4, $respCorrecta)
+  function imprimirImagenRespuestas(int $respuestas, $r1, $r2, $r3, $r4, $respCorrecta)
   {
     $uno = 4 * $respuestas - 3;
     $dos = 4 * $respuestas - 2;
     $tres = 4 * $respuestas - 1;
     $cuatro = 4 * $respuestas;
-    $respuestaNumero=200+$respuestas;
-    $IDvalorCorrecto=300+$respuestas;
+    $respuestaNumero = 200 + $respuestas;
+    $IDvalorCorrecto = 300 + $respuestas;
     echo '
-      <!--+++++++++++++++++++++++++++++++++++++++IMAGEN++++++++++++++++++++++++++++++++++++++++++++-->
-      <div class="container" style="display:none" id ="'.$respuestaNumero.'">
-        <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-            <img src="../CSSsJSs/images/problemaFisica.jpg" class="imagenPregunta" />
-            <p id="'.$IDvalorCorrecto.'">
-              '.$respCorrecta.'
-            </p>
-          </div>
-          <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 col-xl-3">
-            <button class="Opcion1" id="' . $uno . '">
-              ' . $r1 . '
-            </button><br>
-            <button class="Opcion3" id="' . $dos . '">
-              ' . $r2 . '
-            </button>
-          </div>
-          <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 col-xl-3">
-            <button class="Opcion2" id="' . $tres . '">
-              ' . $r3 . '
-            </button><br>
-            <button class="Opcion4" id="' . $cuatro . '">
-              ' . $r4 . '
-            </button>
-          </div>
+    <!--+++++++++++++++++++++++++++++++++++++++IMAGEN++++++++++++++++++++++++++++++++++++++++++++-->
+    <div class="container" style="display:none" id ="' . $respuestaNumero . '">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+          <img src="../CSSsJSs/images/problemaFisica.jpg" class="imagenPregunta" />
+          <p id="' . $IDvalorCorrecto . '">
+            ' . $respCorrecta . '
+          </p>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+          <button class="Opcion1" id="' . $uno . '">
+            ' . $r1 . '
+          </button><br>
+          <button class="Opcion3" id="' . $dos . '">
+            ' . $r2 . '
+          </button>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+          <button class="Opcion2" id="' . $tres . '">
+            ' . $r3 . '
+          </button><br>
+          <button class="Opcion4" id="' . $cuatro . '">
+            ' . $r4 . '
+          </button>
         </div>
       </div>
-    ';
+    </div>
+  ';
   }
   function imprimirFooter()
   {
     echo '
-      <footer class="popUp animated bounceInUp" id="sprintNext" style="display: none;">
-        <div class="container">
-          <div class="row text-center">
-            <div class="hidden-xs hidden-sm col-md-4 col-lg-4 col-xl-4"></div>
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-              <button class="botonContinuar fondoNeutro">Continuar</button>
-            </div>
-            <div class="hidden-xs hidden-sm col-md-4 col-lg-4 col-xl-4"></div>
+    <footer class="popUp animated bounceInUp" id="sprintNext" style="display: none;">
+      <div class="container">
+        <div class="row text-center">
+          <div class="hidden-xs hidden-sm col-md-4 col-lg-4 col-xl-4"></div>
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <button class="botonContinuar fondoNeutro">Continuar</button>
           </div>
+          <div class="hidden-xs hidden-sm col-md-4 col-lg-4 col-xl-4"></div>
         </div>
-      </footer>
-    ';
+      </div>
+    </footer>
+  ';
   }
 
 
