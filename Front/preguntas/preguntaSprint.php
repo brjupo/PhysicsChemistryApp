b4c8274d1e2e290fd059fd55255a0dd88bbf2168
cmendezv <cmendez222@gmail.com>
2020-04-09 12:00:43 -0500

    correct

--- Front/preguntas/preguntaSprint.php
+++ Front/preguntas/preguntaSprint.php
@@ -15,13 +15,13 @@
 <?php
   $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
   //Traer todas las preguntas
-  $query = "SELECT * FROM pregunta LIMIT 10"; //WHERE TEMA = 'TEMA' AND SUBTEMA = 'SUBTEMA' AND LECCION = 'LECCION'";     
+  $query = "SELECT * FROM pregunta"; //WHERE TEMA = 'TEMA' AND SUBTEMA = 'SUBTEMA' AND LECCION = 'LECCION'";     
   $result = mysqli_query($con, $query);
   //contar Numero de elementos
-  //$query2 = "Select count(*) FROM pregunta WHERE id_leccion = 1"; // WHERE TEMA = 'TEMA' AND SUBTEMA = 'SUBTEMA' AND LECCION = 'LECCION'";
-  //$result2 = mysqli_query($con, $query2);
-  //$total = mysqli_fetch_row($result2);
-  $total = 10;
+  $query2 = "Select count(*) FROM pregunta"; // WHERE TEMA = 'TEMA' AND SUBTEMA = 'SUBTEMA' AND LECCION = 'LECCION'";
+  $result2 = mysqli_query($con, $query2);
+  $total = mysqli_fetch_row($result2);
+  //$total = 10;
   //Recorrer el arreglo
   while ($row = mysqli_fetch_assoc($result)) {
     $array[] = $row;
@@ -29,7 +29,7 @@
   }
   $respuestas = array('respuesta_correcta', 'respuesta2', 'respuesta3', 'respuesta4');
   $respuestasr = array('respuesta_correcta', 'respuesta2', 'respuesta3', 'respuesta4');
-  for ($j = 0; $j < $total; $j++) {
+  for ($j = 0; $j < $total[0]; $j++) {
     $i = 0;
     shuffle($respuestas);
     foreach ($respuestas as $val) {
