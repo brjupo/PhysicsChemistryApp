52413bbd737dd30839b6a2d71ff3d645553906b2
cmendezv <cmendez222@gmail.com>
2020-04-09 14:22:26 -0500

    respuestacorrecta

--- Front/preguntas/preguntaTipo2.php
+++ Front/preguntas/preguntaTipo2.php
@@ -69,7 +69,7 @@
         //Se imprime las siguientes preguntas INVISIBLES
         for ($x = 0; $x < $total[0]; $x++) {
             imprimirPregunta($x + 1, $arrayr[$x]["pregunta"]);
-            imprimirImagenRespuestasTipo2($x + 1, $rc);
+            imprimirImagenRespuestasTipo2($x + 1, $arrayr[$x]["respuesta_correcta"]);
         }
     }
     ?>
