94dc790d3396390d43f91c1b62487ace20348590
brjupo <brjupo@gmail.com>
2020-04-09 19:30:34 -0500

    asd

--- Front/preguntas/preguntaTipo2.php
+++ Front/preguntas/preguntaTipo2.php
@@ -140,7 +140,7 @@
         $respuestaNumero = 2000 + $respuestas;
         $IDvalorCorrecto = 3000 + $respuestas;
         $path="../imagenes/" . $imagen . ".jpg";
-        echo '<p>'.$path.'</p>';
+        //echo '<p>'.$path.'</p>';
         if(file_exists($path)){
             echo '
                 <!--+++++++++++++++++++++++++++++++++++++++IMAGEN++++++++++++++++++++++++++++++++++++++++++++-->
