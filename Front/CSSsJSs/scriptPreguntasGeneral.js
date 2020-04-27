1db9cc2cde60b66efb6f77854121f331e9317274
brjupo <brjupo@gmail.com>
2020-04-26 21:07:02 -0500

    asd

--- Front/CSSsJSs/scriptPreguntasGeneral.js
+++ Front/CSSsJSs/scriptPreguntasGeneral.js
@@ -65,7 +65,7 @@ document.addEventListener("click", function (evt) {
 function enviarCalificacion() {
   var userID= document.getElementById("userID").innerHTML.trim();
   var leccionID= document.getElementById("leccionID").innerHTML.trim();
-  alert(userID+ " "+ puntos+ " "+ leccionID);
+  //alert(userID+ " "+ puntos+ " "+ leccionID);
 
   $.ajax({
     type: "POST",
