e0c73e8c1841b09f924d7698ac2c232b13126f00
cmendezv <cmendez222@gmail.com>
2020-05-01 12:56:06 -0500

    preguntas4vlv

---
+++ Front/CSSsJSs/scriptPreguntas4.js
@@ -0,0 +1,78 @@
+document.addEventListener("click", function (evt) {
+    var goButton = document.getElementById("goButton");
+    targetElement = evt.target; // clicked element
+  
+    do {
+      if (targetElement == goButton) {
+        getQuestionMatrix();
+        return;
+      }
+      // Go up the DOM
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+  });
+
+function getQuestionMatrix() {    
+    var leccionID = document.getElementById("leccionID").value.trim();
+    var Usuario = document.getElementById("Usuario").value.trim();
+    var Password = document.getElementById("Password").value.trim();
+    alert(Usuario+ " "+ Password+ " "+ leccionID);
+  
+    $.ajax({
+      type: "POST",
+      url: "../../Servicios/preguntasGral.php",
+      dataType: "json",
+      //data: {leccion: leccionID, userID: Usuario, pass:Password},
+      data: {idLeccion: leccionID},
+      success: function (data) {
+        
+            console.log(data);
+            document.getElementById("ID_pregunta").innerHTML=data[2]["id_pregunta"];
+            document.getElementById("Tipo").innerHTML=1;
+            document.getElementById("PreguntaParte1").innerHTML=1;
+            document.getElementById("PreguntaParte2").innerHTML=1;
+            document.getElementById("R1").innerHTML=1;
+            document.getElementById("R2").innerHTML=1;
+            document.getElementById("R3").innerHTML=1;
+            document.getElementById("R4").innerHTML=1;
+            document.getElementById("RC_Num").innerHTML=1;
+
+            document.getElementById("1ID_pregunta").innerHTML=1;
+            document.getElementById("1Tipo").innerHTML=1;
+            document.getElementById("1PreguntaParte1").innerHTML=1;
+            document.getElementById("1PreguntaParte2").innerHTML=1;
+            document.getElementById("1R1").innerHTML=1;
+            document.getElementById("1R2").innerHTML=1;
+            document.getElementById("1R3").innerHTML=1;
+            document.getElementById("1R4").innerHTML=1;
+            document.getElementById("1RC_Num").innerHTML=1;
+
+
+            document.getElementById("2ID_pregunta").innerHTML=1;
+            document.getElementById("2Tipo").innerHTML=1;
+            document.getElementById("2PreguntaParte1").innerHTML=1;
+            document.getElementById("2PreguntaParte2").innerHTML=1;
+            document.getElementById("2R1").innerHTML=1;
+            document.getElementById("2R2").innerHTML=1;
+            document.getElementById("2R3").innerHTML=1;
+            document.getElementById("2R4").innerHTML=1;
+            document.getElementById("2RC_Num").innerHTML=1;
+
+            //document.getElementById("").innerHTML=1;
+      },
+    });
+  }
+
+
+  /*
+  document.getElementById("leccionID").innerHTML
+  <p id="ID_pregunta"></p>
+<p id="Tipo"></p>
+<p id="PreguntaParte1"></p>
+<p id="PreguntaParte2"></p>
+<p id="R1"></p>
+<p id="R2"></p>
+<p id="R3"></p>
+<p id="R4"></p>
+<p id="RC_Num"></p>
+  */
