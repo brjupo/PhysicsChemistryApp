30c03e2af6c064316d6fdea6f50a7ca5457fb90a
brjupo <brjupo@gmail.com>
2020-05-01 11:26:27 -0500

    Doc prueba de servcio de matriz de preguntas

---
+++ Front/CSSsJSs/scriptPreguntas.js
@@ -0,0 +1,113 @@
+
+document.addEventListener("click", function (evt) {
+    var goButton = document.getElementById("goButton");
+    targetElement = evt.target; // clicked element
+  
+    do {
+      if (targetElement == goButton) {
+        getQuestionMatrix();
+        return;
+      }
+      // Go up the DOM
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+  });
+
+function getQuestionMatrix() {    
+    var leccionID = document.getElementById("leccionID").innerHTML.trim();
+    var Usuario = document.getElementById("Usuario").innerHTML.trim();
+    var Password = document.getElementById("Password").innerHTML.trim();
+    alert(Usuario+ " "+ Password+ " "+ leccionID);
+  
+    $.ajax({
+      type: "POST",
+      url: "../../Servicios/subirPuntos.php",
+      dataType: "json",
+      data: {leccion: leccionID, userID: Usuario, pass:Password},
+      success: function (data) {
+        console.log(data.response);
+        if (data.response == "exito") {
+            /* 
+            document.getElementById("ID_pregunta").innerHTML=1;
+            document.getElementById("Tipo").innerHTML=1;
+            document.getElementById("PreguntaParte1").innerHTML=1;
+            document.getElementById("PreguntaParte2").innerHTML=1;
+            document.getElementById("R1").innerHTML=1;
+            document.getElementById("R2").innerHTML=1;
+            document.getElementById("R3").innerHTML=1;
+            document.getElementById("R4").innerHTML=1;
+            document.getElementById("RC_Num").innerHTML=1;
+
+            document.getElementById("1ID_pregunta").innerHTML=1;
+            document.getElementById("1Tipo").innerHTML=1;
+            document.getElementById("1PreguntaParte1").innerHTML=1;
+            document.getElementById("1PreguntaParte2").innerHTML=1;
+            document.getElementById("1R1").innerHTML=1;
+            document.getElementById("1R2").innerHTML=1;
+            document.getElementById("1R3").innerHTML=1;
+            document.getElementById("1R4").innerHTML=1;
+            document.getElementById("1RC_Num").innerHTML=1;
+
+
+            document.getElementById("2ID_pregunta").innerHTML=1;
+            document.getElementById("2Tipo").innerHTML=1;
+            document.getElementById("2PreguntaParte1").innerHTML=1;
+            document.getElementById("2PreguntaParte2").innerHTML=1;
+            document.getElementById("2R1").innerHTML=1;
+            document.getElementById("2R2").innerHTML=1;
+            document.getElementById("2R3").innerHTML=1;
+            document.getElementById("2R4").innerHTML=1;
+            document.getElementById("2RC_Num").innerHTML=1;
+
+            //document.getElementById("").innerHTML=1;
+            */
+        } else {
+            alert("Algo malo sucedio");
+            document.getElementById("ID_pregunta").innerHTML=1;
+            document.getElementById("Tipo").innerHTML=1;
+            document.getElementById("PreguntaParte1").innerHTML=1;
+            document.getElementById("PreguntaParte2").innerHTML=1;
+            document.getElementById("R1").innerHTML=1;
+            document.getElementById("R2").innerHTML=1;
+            document.getElementById("R3").innerHTML=1;
+            document.getElementById("R4").innerHTML=1;
+            document.getElementById("RC_Num").innerHTML=1;
+
+            document.getElementById("1ID_pregunta").innerHTML=1;
+            document.getElementById("1Tipo").innerHTML=1;
+            document.getElementById("1PreguntaParte1").innerHTML=1;
+            document.getElementById("1PreguntaParte2").innerHTML=1;
+            document.getElementById("1R1").innerHTML=1;
+            document.getElementById("1R2").innerHTML=1;
+            document.getElementById("1R3").innerHTML=1;
+            document.getElementById("1R4").innerHTML=1;
+            document.getElementById("1RC_Num").innerHTML=1;
+
+
+            document.getElementById("2ID_pregunta").innerHTML=1;
+            document.getElementById("2Tipo").innerHTML=1;
+            document.getElementById("2PreguntaParte1").innerHTML=1;
+            document.getElementById("2PreguntaParte2").innerHTML=1;
+            document.getElementById("2R1").innerHTML=1;
+            document.getElementById("2R2").innerHTML=1;
+            document.getElementById("2R3").innerHTML=1;
+            document.getElementById("2R4").innerHTML=1;
+            document.getElementById("2RC_Num").innerHTML=1;
+        }
+      },
+    });
+  }
+
+
+  /*
+  document.getElementById("leccionID").innerHTML
+  <p id="ID_pregunta"></p>
+<p id="Tipo"></p>
+<p id="PreguntaParte1"></p>
+<p id="PreguntaParte2"></p>
+<p id="R1"></p>
+<p id="R2"></p>
+<p id="R3"></p>
+<p id="R4"></p>
+<p id="RC_Num"></p>
+  */
---
+++ Front/preguntas/preguntas.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" type="image/x-icon" href="../CSSsJSs/icons/pyramid.svg" />
+    <title>Pregunta</title>
+    <link rel="stylesheet" href="../CSSsJSs/bootstrap341.css" />
+    <link rel="stylesheet" href="../CSSsJSs/stylePreguntas2.css" />
+    <script src="../CSSsJSs/scriptPreguntas.js"></script>
+    <script src="../CSSsJSs/minAJAX.js"></script>
+</head>
+
+
+<body>
+
+
+    <label for="leccionID">ID Leccion</label>
+    <input type="text" id="leccionID"><br><br>
+    <label for="Usuario">Usuario</label>
+    <input type="text" id="Usuario"><br><br>
+    <label for="Password">Password</label>
+    <input type="text" id="Password"><br><br>
+
+    <h1>Pregunta [0]</h1>
+    <p id="ID_pregunta"></p>
+    <p id="Tipo"></p>
+    <p id="PreguntaParte1"></p>
+    <p id="PreguntaParte2"></p>
+    <p id="R1"></p>
+    <p id="R2"></p>
+    <p id="R3"></p>
+    <p id="R4"></p>
+    <p id="RC_Num"></p>
+
+    <h1>Pregunta [1]</h1>
+    <p id="1ID_pregunta"></p>
+    <p id="1Tipo"></p>
+    <p id="1PreguntaParte1"></p>
+    <p id="1PreguntaParte2"></p>
+    <p id="1R1"></p>
+    <p id="1R2"></p>
+    <p id="1R3"></p>
+    <p id="1R4"></p>
+    <p id="1RC_Num"></p>
+
+    <h1>Pregunta [2]</h1>
+    <p id="2ID_pregunta"></p>
+    <p id="2Tipo"></p>
+    <p id="2PreguntaParte1"></p>
+    <p id="2PreguntaParte2"></p>
+    <p id="2R1"></p>
+    <p id="2R2"></p>
+    <p id="2R3"></p>
+    <p id="2R4"></p>
+    <p id="2RC_Num"></p>
+
+</body>
+
+</html>
