a358e8dec7aaf680215af281e33611415fd03b4f
brjupo <brjupo@gmail.com>
2020-05-01 11:41:28 -0500

    js1

--- Front/preguntas/preguntas.php
+++ Front/preguntas/preguntas.php
@@ -8,7 +8,7 @@
     <title>Pregunta</title>
     <link rel="stylesheet" href="../CSSsJSs/bootstrap341.css" />
     <link rel="stylesheet" href="../CSSsJSs/stylePreguntas2.css" />
-    <script src="../CSSsJSs/scriptPreguntas.js"></script>
+    <script src="../CSSsJSs/scriptPreguntas1.js"></script>
     <script src="../CSSsJSs/minAJAX.js"></script>
 </head>
 
