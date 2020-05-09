79f71ad052d1d6740a66d924a0f7165ad767037a
brjupo <brjupo@gmail.com>
2020-05-09 16:53:12 -0500

    njsadadslksad

--- Front/errorInfoPages/uploadInfo.php
+++ Front/errorInfoPages/uploadInfo.php
@@ -1,3 +1,30 @@
+<?php
+if (isset($_POST["Import"])) {
+  $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
+  $filename = $_FILES["file"]["tmp_name"];
+  if ($_FILES["file"]["size"] > 0) {
+    $file = fopen($filename, "r");
+    while (($getData = fgetcsv($file, 10000, ",")) !== FALSE) {
+      $sql = "INSERT into usuario_prueba (mail,pswd) 
+                   values ('" . $getData[0] . "','" . $getData[1] . "')";
+      $result = mysqli_query($con, $sql);
+      if (!isset($result)) {
+        echo "<script type=\"text/javascript\">
+              alert(\"Invalid File:Please Upload CSV File.\");
+              window.location = \"index.php\"
+              </script>";
+      } else {
+        echo "<script type=\"text/javascript\">
+            alert(\"CSV File has been successfully Imported.\");
+            window.location = \"index.php\"
+          </script>";
+      }
+    }
+    fclose($file);
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html>
 
