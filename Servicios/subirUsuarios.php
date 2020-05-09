f312d2fc925edbdc16c44e223ffc73a6969298cd
brjupo <brjupo@gmail.com>
2020-05-09 17:06:15 -0500

    jasddjsad

--- Servicios/subirUsuarios.php
+++ Servicios/subirUsuarios.php
@@ -1,36 +1,30 @@
 <?php
 
 
-if(isset($_POST["Import"])){
-    
-$con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
+if (isset($_POST["Import"])) {
 
-    $filename=$_FILES["file"]["tmp_name"];    
-     if($_FILES["file"]["size"] > 0)
-     {
-        $file = fopen($filename, "r");
-          while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
-           {
-             $sql = "INSERT into usuario_prueba (mail,pswd) 
-                   values ('".$getData[0]."','".$getData[1]."')";
-                   $result = mysqli_query($con, $sql);
-        if(!isset($result))
-        {
-          echo "<script type=\"text/javascript\">
+  $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
+
+  $filename = $_FILES["file"]["tmp_name"];
+  if ($_FILES["file"]["size"] > 0) {
+    $file = fopen($filename, "r");
+    while (($getData = fgetcsv($file, 10000, ",")) !== FALSE) {
+      $sql = "INSERT into usuario_prueba (mail,pswd) 
+                   values ('" . $getData[0] . "','" . $getData[1] . "')";
+      $result = mysqli_query($con, $sql);
+      if (!isset($result)) {
+        echo "<script type=\"text/javascript\">
               alert(\"Invalid File:Please Upload CSV File.\");
               window.location = \"../Front/errorInfoPages/uploadInfo.php\"
-              </script>";    
-        }
-        else {
-            echo "<script type=\"text/javascript\">
+              </script>";
+      } else {
+        echo "<script type=\"text/javascript\">
             alert(\"CSV File has been successfully Imported.\");
             window.location = \"../Front/errorInfoPages/uploadInfo.php\"
           </script>";
-        }
-           }
-      
-           fclose($file);  
-     }
-  }   
+      }
+    }
 
-?>
+    fclose($file);
+  }
+}
