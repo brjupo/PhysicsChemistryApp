c5cb47e7021ebd43004c7e8efdc864c547dd6714
brjupo <brjupo@gmail.com>
2020-05-28 21:32:31 -0500

    temas2

---
+++ Front/CSSsJSs/Obsoletos/scriptTemas.js
@@ -0,0 +1,35 @@
+
+var nodoPadre = null;
+var nombreImagen;
+
+window.onload = function () {
+}
+
+document.addEventListener("click", function (evt) {
+    var profile = document.getElementById("botonPerfil");
+    var logout = document.getElementById("botonLogout");
+    targetElement = evt.target; // clicked element
+    elID= targetElement.id;
+
+    do {
+        if (targetElement == profile) {
+            profilev();
+            return;
+        }
+        if (targetElement == logout) {
+            do_logout();
+            return;
+        }
+        // Go up the DOM
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+});
+
+function profilev() {
+    var asignatura = document.getElementById("asignatura").innerHTML;
+    var url = "perfil.php?asignatura=";
+    location.replace(url.concat(asignatura));
+}
+function do_logout(){
+    location.replace("../../index.php");
+}
