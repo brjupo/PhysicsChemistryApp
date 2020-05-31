af2c2e4848c2bd360150ea1a17bb24647efbcd12
cmendezv <cmendez222@gmail.com>
2020-05-31 03:12:12 -0500

    ssss

--- Front/Inicio/lecciones.php
+++ Front/Inicio/lecciones.php
@@ -338,45 +338,6 @@
   {
 
     if($habilitar == '1'){
-      if($numeroLeccion == $_SESSION["tamanhoh"]){////HERE
-        echo '
-        <div class="container">
-          <div id="seccion' . $numeroLeccion . '" class="row fade" style="opacity:0.0">
-            <div class="textCenter col-xs-0 col-sm-0 col-md-1 col-lg-2 col-xl-2"></div>
-            <div class="temaPrincipal1 textCenter col-xs-12 col-sm-12 col-md-10 col-lg-8 col-xl-8">
-              <table class="table fixed">
-                <tbody>
-                  <tr>
-                    <td>
-                      <img class="iconsNumber" src="../CSSsJSs/icons/' . $numeroLeccion . '.svg" />
-                    </td>
-                    <td class="tituloTemasPrincipales">
-                    ' . $nombreLeccion . '
-                    </td>
-                    <td>
-                    <a href="../preguntas/practice.php?leccion='.$nombreLeccion.'"><img class="iconsActive" src="../CSSsJSs/icons/book.svg" /></a>
-                    </td>
-                    <td>
-                      <img class="icons" src="../CSSsJSs/icons/jogging.svg" /></a>
-                    </td>
-                    <td>
-                      <img class="icons" src="../CSSsJSs/icons/examen.svg" />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="textCenter col-xs-0 col-sm-0 col-md-1 col-lg-2 col-xl-2"></div>
-          </div>
-        </div>
-
-        <div class="container">
-          <div class="row">
-            <p></p>
-          </div>
-        </div>
-    ';
-      }else{
       /////////////
     echo '
       <div class="container">
@@ -415,8 +376,6 @@
         </div>
       </div>
   ';}
-
-}
   else{
           echo '
               <div class="container">
