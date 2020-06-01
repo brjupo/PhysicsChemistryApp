6c473664a801d30df40381409294c20424dd0ce3
brjupo <brjupo@gmail.com>
2020-05-31 20:19:37 -0500

    asd

--- Front/preguntas/sprintFinalizado.php
+++ Front/preguntas/sprintFinalizado.php
@@ -14,8 +14,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="../CSSsJSs/icons/pyramid.svg" />
     <title>Kaanbal</title>
     <link rel="stylesheet" href="../CSSsJSs/bootstrap341.css" />
-    <link rel="stylesheet" href="../CSSsJSs/stylePreguntas9.css" />
-    <link rel="stylesheet" href="../CSSsJSs/styleNivelCompletado.css" />
+    <link rel="stylesheet" href="../CSSsJSs/styleNivelCompletado2.css" />
     <script src="../CSSsJSs/scriptSprintFinalizado2.js"></script>
   </head>
 
