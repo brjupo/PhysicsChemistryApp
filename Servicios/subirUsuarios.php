22e61cea328572b30aeb18c4d9ea81125b00ade3
cmendezv <cmendez222@gmail.com>
2020-06-04 23:02:50 -0500

    jajajaa

--- Servicios/subirUsuarios.php
+++ Servicios/subirUsuarios.php
@@ -18,7 +18,8 @@ if (isset($_POST["Import"])) {
       //Corroborar que no existe el correo en base de datos
       $sql = "SELECT mail FROM usuario_prueba WHERE mail = '$mailr'";
       $resultp = mysqli_query($con, $sql);
-      $rowp = mysqli_fetch_array($resultp);
+      //$rowp = mysqli_fetch_array($resultp);
+      $rowp = 0;
 
       if ($rowp) {
 
@@ -36,8 +37,22 @@ if (isset($_POST["Import"])) {
 
       }else{
 
-      $sql = "INSERT into usuario_prueba (mail,pswd) 
+      /* $sql = "INSERT into usuario_prueba (mail,pswd) 
                    values ('" . $getData[0] . "@itesm.mx','" . $getData[1] . "')";
+      $result = mysqli_query($con, $sql);
+      if (!isset($result)) {
+        echo "<script type=\"text/javascript\">
+              alert(\"Invalid File:Please Upload CSV File.\");
+              window.location = \"../Front/errorInfoPages/uploadInfo.php\"
+              </script>";
+      } else {
+        echo "<script type=\"text/javascript\">
+            alert(\"CSV File has been successfully Imported.\");
+            window.location = \"../Front/errorInfoPages/uploadInfo.php\"
+          </script>";
+      } */
+      $sql = "INSERT into alumno (id_usuario,matricula) 
+                   values ('" . $getData[0] . "','" . $getData[1] . "')";
       $result = mysqli_query($con, $sql);
       if (!isset($result)) {
         echo "<script type=\"text/javascript\">
