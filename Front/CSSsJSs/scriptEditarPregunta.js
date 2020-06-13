677512e8817c76fa584095fe23e45dd821c56289
brjupo <brjupo@gmail.com>
2020-06-13 17:57:52 -0500

    Prueba leer preguntas de la BBDD

---
+++ Front/CSSsJSs/scriptEditarPregunta.js
@@ -0,0 +1,53 @@
+document.addEventListener("click", function (evt) {
+  var cargarInformacion = document.getElementById("cargarInformacion");
+  var guardarEnBBDD = document.getElementById("guardarEnBBDD");
+  targetElement = evt.target; // clicked element
+
+  do {
+    if (targetElement == cargarInformacion) {
+      loadInformation();
+      return;
+    }
+    if (targetElement == guardarEnBBDD) {
+      //saveInDDBB();
+      return;
+    }
+    // Go up the DOM
+    targetElement = targetElement.parentNode;
+  } while (targetElement);
+});
+
+function saveInDDBB() {}
+
+function loadInformation() {
+  $.ajax({
+    type: "POST",
+    url: "../../Servicios/getQuestionInfo.php",
+    dataType: "json",
+    data: {
+      leccion: document.getElementById("idLeccion").value,
+      numeroPregunta: document.getElementById("numeroPregunta").value,
+    },
+    success: function (data) {
+      console.log(data.response);
+      if (data.response == "exito") {
+        alert("Información obtenida");
+        showData(data);
+      } else {
+        alert(data.response);
+      }
+    },
+    error: function () {
+      alert("ERROR Desconocido");
+    },
+  });
+}
+
+function showData(data) {
+  document.getElementById("pregunta").value = data.pregunta;
+  document.getElementById("respuesta_correcta").value = data.respuesta_correcta;
+  document.getElementById("respuesta2").value = data.respuesta2;
+  document.getElementById("respuesta3").value = data.respuesta3;
+  document.getElementById("respuesta4").value = data.respuesta4;
+  document.getElementById("tipo").value = data.tipo;
+}
