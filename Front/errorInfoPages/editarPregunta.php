3a8d50bc577550e17999a97ec2fc04918c308fc5
cmendezv <cmendez222@gmail.com>
2020-06-13 22:35:39 -0500

    jejeje

--- Front/errorInfoPages/editarPregunta.php
+++ Front/errorInfoPages/editarPregunta.php
@@ -21,20 +21,34 @@
   //////////////////////////////////////////////////////
   session_start();
   $tokenValidar = array();
+  $idValidarprofe = array();
 
   //Consultar si existe token de usuario
-  $statement = mysqli_prepare($con, "SELECT tokenSesion FROM usuario_prueba WHERE mail = ?");
+  $statement = mysqli_prepare($con, "SELECT tokenSesion, id_usuario FROM usuario_prueba WHERE mail = ?");
   mysqli_stmt_bind_param($statement, "s", $_SESSION["mail"]);
   mysqli_stmt_execute($statement);
 
   mysqli_stmt_store_result($statement);
-  mysqli_stmt_bind_result($statement, $tokenSesionp);
+  mysqli_stmt_bind_result($statement, $tokenSesionp, $iduser);
 
   while (mysqli_stmt_fetch($statement)) {
+    $idValidarprofe["profe"] = $iduser;
     $tokenValidar["tokenSesionp"] = $tokenSesionp;
   }
 
-  if ($_SESSION["tokenSesion"] == $tokenValidar["tokenSesionp"] and $tokenValidar["tokenSesionp"] != "") {
+  //Consultar si es profe
+  $statement = mysqli_prepare($con, "SELECT id_profesor FROM profesor WHERE id_usuario = ?");
+  mysqli_stmt_bind_param($statement, "s", $idValidarprofe["profe"]);
+  mysqli_stmt_execute($statement);
+
+  mysqli_stmt_store_result($statement);
+  mysqli_stmt_bind_result($statement, $idProfe);
+
+  while (mysqli_stmt_fetch($statement)) {
+    $existeProfe["profe"] = $idProfe;
+  }
+
+  if ($_SESSION["tokenSesion"] == $tokenValidar["tokenSesionp"] and $existeProfe["profe"] != "" and $tokenValidar["tokenSesionp"] != "") {
             echo'<div class="container">
             <div class="row">
             <div class="textCenter col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
