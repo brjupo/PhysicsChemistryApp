63f7e788d13fa6a6881c9f9cee0428b649c1761d
brjupo <brjupo@gmail.com>
2020-06-13 23:09:40 -0500

    asdad

--- Front/Inicio/temas.php
+++ Front/Inicio/temas.php
@@ -194,7 +194,7 @@
         <div class="row">
           <div class="textCenter col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
           <div class="textCenter col-xs-10 col-sm-8 col-md-6 col-lg-4 col-xl-4">
-            <p class="cita">Página en mantenimiento, puedes experimentar dificultades</p>
+            <p class="cita"></p>
           </div>
           <div class="textCenter col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
         </div>
