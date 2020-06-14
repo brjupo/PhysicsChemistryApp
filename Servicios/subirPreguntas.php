781f322d0157c763c0d09f860a90c4c82613b9f9
cmendezv <cmendez222@gmail.com>
2020-06-13 19:05:48 -0500

    ssss

--- Servicios/subirPreguntas.php
+++ Servicios/subirPreguntas.php
@@ -15,28 +15,26 @@ if (isset($_POST["Import"])) {
               //$idp = $getData[0];
 
               //Corroborar que no existe el id de pregunta en base de datos
-      $sql = "SELECT id_pregunta FROM pregunta WHERE id_pregunta = '$idp'";
+      $sql = "SELECT pregunta FROM pregunta WHERE id_pregunta = '$idp'";
       $resultp = mysqli_query($con, $sql);
       $rowp = mysqli_fetch_array($resultp);
          
-       if (isset($rowp)) {
-
-      } else{
-      $sql = "INSERT into pregunta (id_pregunta, id_autor, id_leccion, orden, pregunta, respuesta_correcta, respuesta2, respuesta3, respuesta4, tipo, estatus, tiene_imagen) 
-                   values ('" . $getData[0] ."','" . $getData[1] . "','" . $getData[2] ."','" . $getData[3] ."','" . $getData[4] ."','" . $getData[5] ."','" . $getData[6] ."','" . $getData[7] ."','" . $getData[8] ."','" . $getData[9] ."','" . $getData[10] ."','" . $getData[11] ."')";
-      $result = mysqli_query($con, $sql);
-      if (!isset($result)) {
-        echo "<script type=\"text/javascript\">
+       if (!isset($rowp)) {
+            $sql = "INSERT into pregunta (id_pregunta, id_autor, id_leccion, orden, pregunta, respuesta_correcta, respuesta2, respuesta3, respuesta4, tipo, estatus, tiene_imagen) 
+            values ('" . $getData[0] ."','" . $getData[1] . "','" . $getData[2] ."','" . $getData[3] ."','" . $getData[4] ."','" . $getData[5] ."','" . $getData[6] ."','" . $getData[7] ."','" . $getData[8] ."','" . $getData[9] ."','" . $getData[10] ."','" . $getData[11] ."')";
+            $result = mysqli_query($con, $sql);
+            if (!isset($result)) {
+            echo "<script type=\"text/javascript\">
               alert(\"Archivo no valido: Por favor carga un archivo CSV.\");
               window.location = \"../Front/errorInfoPages/uploadPreguntas.php\"
               </script>";
-      } else {
-        echo "<script type=\"text/javascript\">
+            } else {
+            echo "<script type=\"text/javascript\">
             alert(\"Las preguntas se han cargado correctamente.\");
             window.location = \"../Front/errorInfoPages/uploadPreguntas.php\"
-          </script>";
-      }
-     }
+            </script>";
+            }
+      } 
     }
 
     fclose($file);
