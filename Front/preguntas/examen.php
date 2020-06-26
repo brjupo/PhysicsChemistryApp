af850987c95200936c6ced6879176fc44a06f733
cmendezv <cmendez222@gmail.com>
2020-06-26 16:25:06 -0500

    ddddd

--- Front/preguntas/examen.php
+++ Front/preguntas/examen.php
@@ -259,12 +259,22 @@
     function imprimirTiempoexamen()
     {
         echo '
-                
                 <div class="container">
                 <div class="row">
                 <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
                 <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                    <p id="number"></p>
+                <table class="table fixed">
+                <tbody>
+                  <tr>
+                    <td>
+                      <p id="number"></p>
+                    </td>
+                    <td>
+                      <img class="icons" src="../CSSsJSs/icons/examen.svg" />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
                 </div>
                 <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
                 </div>
