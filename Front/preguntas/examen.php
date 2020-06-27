d81571795f4d586478ca2bfa1150fcb07ebab82e
cmendezv <cmendez222@gmail.com>
2020-06-26 20:50:49 -0500

    helo

--- Front/preguntas/examen.php
+++ Front/preguntas/examen.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="../CSSsJSs/icons/pyramid.svg" />
     <title>Pregunta</title>
     <link rel="stylesheet" href="../CSSsJSs/bootstrap341.css" />
-    <link rel="stylesheet" href="../CSSsJSs/styleExamen.css" />
+    <link rel="stylesheet" href="../CSSsJSs/styleExamenn.css" />
     <script src="../CSSsJSs/scriptExamen10.js"></script>
     <script src="../CSSsJSs/minAJAX.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
