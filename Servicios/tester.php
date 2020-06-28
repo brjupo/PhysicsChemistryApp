85272e3b911fe7d24bcbe4e508dc94850d023610
cmendezv <cmendez222@gmail.com>
2020-06-27 22:34:23 -0500

    testerasignaturas

--- Servicios/tester.php
+++ Servicios/tester.php
@@ -1,27 +1,59 @@
 <?php
   $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
-
+  //////////////////////////////////////////////////////
   session_start();
+  $tokenValidar = array();
+  $idValidarprofe = array();
 
-  $iduser = $_SESSION["id_usuario"];
+  //Consultar si existe token de usuario
+  $statement = mysqli_prepare($con, "SELECT tokenSesion, id_usuario FROM usuario_prueba WHERE mail = ?");
+  mysqli_stmt_bind_param($statement, "s", $_SESSION["mail"]);
+  mysqli_stmt_execute($statement);
 
-  //Obtener el top 5 de alumnos con mayor puntuación
-    $statement = mysqli_prepare($con, "SELECT * FROM alumno a INNER JOIN (SELECT id_usuario, SUM(puntuacion) AS suma FROM puntuacion WHERE id_leccion IN (SELECT id_leccion FROM leccion WHERE id_subtema IN (SELECT id_subtema FROM subtema WHERE id_tema IN (SELECT id_tema FROM tema WHERE id_asignatura = ?))) GROUP BY id_usuario ORDER BY suma DESC LIMIT 5) p ON a.id_usuario = p.id_usuario WHERE a.id_usuario IN (SELECT id_usuario FROM licencia WHERE vigencia >= NOW() AND estatus = 1)");
-    //[ID DE LA ASIGNATURA ACTUAL]
-    mysqli_stmt_bind_param($statement, "i", $_SESSION["id_usuario"]);
+  mysqli_stmt_store_result($statement);
+  mysqli_stmt_bind_result($statement, $tokenSesionp, $iduser);
+
+  while (mysqli_stmt_fetch($statement)) {
+    $idValidarprofe["profe"] = $iduser;
+    $tokenValidar["tokenSesionp"] = $tokenSesionp;
+  }
+
+  //Consultar si es profe
+  $statement = mysqli_prepare($con, "SELECT id_profesor FROM profesor WHERE id_usuario = ?");
+  mysqli_stmt_bind_param($statement, "s", $idValidarprofe["profe"]);
+  mysqli_stmt_execute($statement);
+
+  mysqli_stmt_store_result($statement);
+  mysqli_stmt_bind_result($statement, $idProfe);
+
+  while (mysqli_stmt_fetch($statement)) {
+    $existeProfe["profe"] = $idProfe;
+  }
+
+  if ($_SESSION["tokenSesion"] == $tokenValidar["tokenSesionp"] and $existeProfe["profe"] != "" and $tokenValidar["tokenSesionp"] != "") {
+
+    $statement = mysqli_prepare($con, "SELECT id_asignatura, nombre FROM asignatura");
     mysqli_stmt_execute($statement);
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $id_alumno, $id_usuario, $matricula, $avatar);
+    mysqli_stmt_bind_result($statement, $id_asignatura, $nombre);
 
-    $arregloTopUsuarios = array();
-
+    $arregloAsignaturas = array();
     $i = 0;
-    while (mysqli_stmt_fetch($statement)) {
-      $arregloAsignaturas[$i]["id_alumno"] = $id_alumno;
-      $arregloAsignaturas[$i]["id_usuario"] = $id_usuario;
-      $arregloAsignaturas[$i]["matricula"] = $matricula;
-      $arregloAsignaturas[$i]["avatar"] = $avatar;
+    //Leemos datos del la leccion habilitadas
+    while (mysqli_stmt_fetch($statement)) { //si si existe la leccion
+      $arregloAsignaturas[$i]["id_asignatura"] = $id_asignatura;
+      $arregloAsignaturas[$i]["nombre"] = $nombre;
       $i = $i + 1;
     }
+  
+    
+
+
+}else{
+  echo '<script type="text/javascript">
+  alert("Inicie sesión");
+  window.location.href="https://kaanbal.net";
+  </script>';
+}
 
 ?>
