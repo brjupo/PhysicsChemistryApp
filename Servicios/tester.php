11f3a533b796bebc8edb63526e0bfc96050491c2
cmendezv <cmendez222@gmail.com>
2020-06-27 23:57:47 -0500

    dsslokia

--- Servicios/tester.php
+++ Servicios/tester.php
@@ -35,7 +35,9 @@ while (mysqli_stmt_fetch($statement)) {
     $idAsignatura = 1;
 
     $statement = mysqli_prepare($con, "SELECT id_tema, nombre FROM tema WHERE id_asignatura = ?");
-    mysqli_stmt_execute($statement,"i", $idAsignatura);
+    mysqli_stmt_bind_param($statement,"i", $idAsignatura);
+    mysqli_stmt_execute($statement);
+    
     mysqli_stmt_store_result($statement);
     mysqli_stmt_bind_result($statement, $id_tema, $nombre);
 
