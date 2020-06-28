731153674563d60b09d3742323757fb93c0aba63
brjupo <brjupo@gmail.com>
2020-06-28 17:42:18 -0500

    control prfo

--- Front/profesor/controlProfesor.php
+++ Front/profesor/controlProfesor.php
@@ -61,6 +61,7 @@ function printTeacherControlPanel()
     </head>
   
     <body>
+    
       <div class="container">
         <div class="row">
           <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
@@ -201,6 +202,7 @@ function printTeacherControlPanel()
           </div>
         </div>
       </div>
+
     </body>
     ';
 }
