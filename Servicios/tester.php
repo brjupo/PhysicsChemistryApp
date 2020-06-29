b08afda8033616a072e846fc3649eda930639262
cmendezv <cmendez222@gmail.com>
2020-06-29 14:37:42 -0500

    misree

--- Servicios/tester.php
+++ Servicios/tester.php
@@ -1,7 +1,9 @@
 <?php
 $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
 //////////////////////////////////////////////////////
-    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (15,9,(NOW()+8))";
+    $date = date('Y-m-j H:i:s');
+    $newDate = strtotime('-5 hour', strtotime($date));
+    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (16,9,$newDate)";
     mysqli_multi_query($con, $sql);
 
 ?>
