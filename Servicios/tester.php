56a6578017ec29bfa5663b129c6685a8405f0af1
cmendezv <cmendez222@gmail.com>
2020-06-29 15:01:23 -0500

    vrasil

--- Servicios/tester.php
+++ Servicios/tester.php
@@ -1,9 +1,18 @@
 <?php
 $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
 //////////////////////////////////////////////////////
-    $date = date('Y-m-j H:i:s');
-    $newDate = strtotime('-5 hour', strtotime($date));
-    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (22,9,'$newDate')";
+
+    $tiempo = getDatetimeNow();
+    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (22,9,$tiempo)";
     mysqli_multi_query($con, $sql);
+
+    function getDatetimeNow() {
+        $tz_object = new DateTimeZone('Brazil/East');
+        //date_default_timezone_set('Brazil/East');
+    
+        $datetime = new DateTime();
+        $datetime->setTimezone($tz_object);
+        return $datetime->format('Y\-m\-d\ h:i:s');
+    }
 
 ?>
