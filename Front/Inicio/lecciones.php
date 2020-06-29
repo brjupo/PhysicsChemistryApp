b612ef42a1918053767859f07cdafc839dd46001
brjupo <brjupo@gmail.com>
2020-06-28 21:53:32 -0500

    asd

--- Front/Inicio/lecciones.php
+++ Front/Inicio/lecciones.php
@@ -166,7 +166,7 @@
     $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
     ///Llamar a las habilitadas
     //Debe de traer todas en la que puntiacion PP sea mayor a 70%, las que sea mayor al 70% deberan tener habilitado el sprint y la siguiente leccion
-    $statement = mysqli_prepare($con, "SELECT l.*, FLOOR(COUNT(p.id_leccion) * 0.7) as setenta, pu.puntuacion FROM leccion l JOIN pregunta p JOIN puntuacion pu ON p.id_leccion = l.id_leccion AND l.id_leccion = pu.id_leccion AND p.id_leccion = pu.id_leccion WHERE l.id_subtema = ? AND pu.id_usuario = ? AND pu.tipo = 'PP' ORDER BY orden ASC GROUP BY p.id_leccion");
+    $statement = mysqli_prepare($con, "SELECT l.*, FLOOR(COUNT(p.id_leccion) * 0.7) as setenta, pu.puntuacion FROM leccion l JOIN pregunta p JOIN puntuacion pu ON p.id_leccion = l.id_leccion AND l.id_leccion = pu.id_leccion AND p.id_leccion = pu.id_leccion WHERE l.id_subtema = ? AND pu.id_usuario = ? AND pu.tipo = 'PP' GROUP BY p.id_leccion");
     mysqli_stmt_bind_param($statement, "ii", $id_subtema, $_SESSION["id_usuario"]);
     mysqli_stmt_execute($statement);
 
@@ -196,7 +196,7 @@
     }
 
     //Llamar no habilitadas
-    $statement = mysqli_prepare($con, "SELECT id_leccion, id_subtema, nombre, orden FROM leccion WHERE id_subtema = ?  ORDER BY orden ASC");
+    $statement = mysqli_prepare($con, "SELECT id_leccion, id_subtema, nombre, orden FROM leccion WHERE id_subtema = ?");
     mysqli_stmt_bind_param($statement, "i", $id_subtema);
     mysqli_stmt_execute($statement);
 
