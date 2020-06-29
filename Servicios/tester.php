a993da9879bbbc7039aae66bc0df7e89911c7f24
cmendezv <cmendez222@gmail.com>
2020-06-29 13:22:47 -0500

    timer

--- Servicios/tester.php
+++ Servicios/tester.php
@@ -1,9 +1,8 @@
 <?php
 $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
 //////////////////////////////////////////////////////
-    
-    $sql = "SET time_zone = '-05:00'";
-    $sql .= "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (13,9,NOW())";
+    date_default_timezone_set('-06:00');
+    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (13,9,NOW())";
     mysqli_multi_query($con, $sql);
 
 ?>
