42fc4fa2cda416bb6b679d2e8b4fe1c7fcbc4fc0
cmendezv <cmendez222@gmail.com>
2020-06-29 00:41:30 -0500

    SSSSSSSAWQ

--- Servicios/tester.php
+++ Servicios/tester.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
 //////////////////////////////////////////////////////
-    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo) values (8,9)";
+    $sql = "INSERT into alumno_grupo (id_alumno,id_grupo,tiempo) values (9,9,NOW())";
     mysqli_query($con, $sql);
 
 ?>
