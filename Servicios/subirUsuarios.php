8f3443d4269098d84b31d49b6d2cf967400b885a
cmendezv <cmendez222@gmail.com>
2020-07-05 00:22:44 -0500

    fechas

--- Servicios/subirUsuarios.php
+++ Servicios/subirUsuarios.php
@@ -66,9 +66,10 @@ if (isset($_POST["Import"])) {
                    values ($ultimoId,'" . $getData[0] . "')";
       $result = mysqli_query($con, $sql);
 
+      $date = date('m/d/Y h:i:s', '12/30/2020 12:00:00');
       //tabla licencia (id_usuario,id_asignatura,codigo,autorizacion,activacion,vigenvia,estatus)
       $sql = "INSERT into licencia (id_usuario,id_asignatura,codigo,autorizacion,activacion,vigencia,estatus) 
-                   values ($ultimoId,'" . $getData[3] . "',$ultimoId,$ultimoId,$ultimoId,'30/06/2022  11:59:59',1)";
+                   values ($ultimoId,'" . $getData[3] . "',$ultimoId,$ultimoId,$ultimoId,$date,1)";
       $result = mysqli_query($con, $sql); 
 
 
