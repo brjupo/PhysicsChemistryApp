95d3e222c851639f3782554f772010297ee6b102
brjupo <brjupo@gmail.com>
2020-07-04 23:45:46 -0500

    asd

--- Front/profesor/controlDvlp.php
+++ Front/profesor/controlDvlp.php
@@ -84,7 +84,7 @@ function printTeacherControlPanel()
       <div class="container">
         <div class="row">
           <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <h4>Panel de profesores</h4>
+            <h4>Panel de Desarrolladores</h4>
           </div>
         </div>
       </div>
