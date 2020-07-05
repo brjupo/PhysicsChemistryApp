9d857f15739faf0298cf0d1ab91ef17ac08d0a39
brjupo <brjupo@gmail.com>
2020-07-04 23:06:05 -0500

    tiempos sprint y examen

---
+++ Front/profesor/SERVICIOS/tiempoExamenLeccion.php
@@ -0,0 +1,30 @@
+<?php
+    //Informacion de la BBDD
+    $servername = "localhost";
+    $username = "u526597556_dev";
+    $password = "1BLeeAgwq1*isgm&jBJe";
+    $dbname = "u526597556_kaanbal";
+
+    //Leer las variables del POST
+    $id_leccion = $_POST['id_leccion'];
+    $tiempo_examen = $_POST['tiempo_examen'];
+
+    //Crear la escritura en base de datos
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')
+        //UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1
+        $sql = "UPDATE leccion SET tiempo_examen = '".$tiempo_examen."' WHERE id_leccion = '".$id_leccion."'";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        $response["response"] = 'exito';
+        //$response["response"] = $sql;
+      } catch(PDOException $e) {
+        $response["response"] = $sql . "<br>" . $e->getMessage();
+      }
+      
+      $conn = null;
+    ////////////////    
+    echo json_encode($response);
---
+++ Front/profesor/tiempoSprintExamen/elegirAsignatura.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<?php
+$con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
+//////////////////////////////////////////////////////
+session_start();
+$tokenValidar = array();
+$idValidarprofe = array();
+
+//Consultar si existe token de usuario
+$statement = mysqli_prepare($con, "SELECT tokenSesion, id_usuario FROM usuario_prueba WHERE mail = ?");
+mysqli_stmt_bind_param($statement, "s", $_SESSION["mail"]);
+mysqli_stmt_execute($statement);
+
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $tokenSesionp, $iduser);
+
+while (mysqli_stmt_fetch($statement)) {
+  $idValidarprofe["profe"] = $iduser;
+  $tokenValidar["tokenSesionp"] = $tokenSesionp;
+}
+
+//Consultar si es profe
+$statement = mysqli_prepare($con, "SELECT id_profesor FROM profesor WHERE id_usuario = ?");
+mysqli_stmt_bind_param($statement, "s", $idValidarprofe["profe"]);
+mysqli_stmt_execute($statement);
+
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $idProfe);
+
+while (mysqli_stmt_fetch($statement)) {
+  $existeProfe["profe"] = $idProfe;
+}
+
+if ($_SESSION["tokenSesion"] == $tokenValidar["tokenSesionp"] and $existeProfe["profe"] != "" and $tokenValidar["tokenSesionp"] != "") {
+  printSubjects();
+} else {
+  echo '<script type="text/javascript">
+  alert("Inicie sesión");
+  window.location.href="https://kaanbal.net";
+  </script>';
+}
+?>
+
+<?php
+
+function printSubjects()
+{
+  echo '
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="shortcut icon"
+      type="image/x-icon"
+      href="../CSSsJSs/icons/pyramid.svg"
+    />
+    <title>Kaanbal</title>
+    <link rel="stylesheet" href="../CSSsJSs/bootstrap441.css" />
+    <link rel="stylesheet" href="../CSSsJSs/kaanbalEsentials.css" />
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+        <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+          <h1 class="titulo">Kaanbal</h1>
+        </div>
+        <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
+        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <p style="color: rgba(0, 0, 0, 0);">.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <p>Elija la asignatura a la que desea cambiar los títulos</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <p style="color: rgba(0, 0, 0, 0);">.</p>
+        </div>
+      </div>
+    </div>
+    ';
+  printSubjectOptions();
+  echo '
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <p style="color: rgba(0, 0, 0, 0);">.</p>
+        </div>
+      </div>
+    </div>
+    ';
+}
+
+function printSubjectOptions(){
+  $con = mysqli_connect("localhost", "u526597556_dev", "1BLeeAgwq1*isgm&jBJe", "u526597556_kaanbal");
+  $statement = mysqli_prepare($con, "SELECT id_asignatura, nombre FROM asignatura");
+  mysqli_stmt_execute($statement);
+  mysqli_stmt_store_result($statement);
+  mysqli_stmt_bind_result($statement, $id_asignatura, $nombre);
+
+  $arregloAsignaturas = array();
+  $i = 0;
+  //Leemos datos del la leccion habilitadas
+  while (mysqli_stmt_fetch($statement)) { //si si existe la leccion
+    $arregloAsignaturas[$i]["id_asignatura"] = $id_asignatura;
+    $arregloAsignaturas[$i]["nombre"] = $nombre;
+    $i = $i + 1;
+  }
+
+  $tamanho = count($arregloAsignaturas);
+
+  for ($i = 0; $i < $tamanho; $i++) {
+    //print_r($arregloAsignaturas[$i]["id_asignatura"]);
+    //print_r($arregloAsignaturas[$i]["nombre"]);
+    echo '
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <a href="elegirTema.php?ID_Asignatura='.$arregloAsignaturas[$i]["id_asignatura"].'">
+            <button type="button" class="btn btn-outline-dark">
+            '.$arregloAsignaturas[$i]["nombre"].'
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <p style="color: rgba(0, 0, 0, 0);">.</p>
+        </div>
+      </div>
+    </div>
+    ';
+  }
+}
+?>
+
+</body>
+
+</html>
