55b3f08fa6b2bbed4b785ea29f211493892ee669
brjupo <brjupo@gmail.com>
2020-07-07 23:39:07 -0500

    asasdasd

--- index.php
+++ index.php
@@ -110,7 +110,8 @@
                 <div class=" row text-center">
                     <div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3 "></div>
                     <div class="textCenter col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                        <a href="contacto.html"><p class="footSubject">Contáctanos</p></a>
+                        <button class="footSubject" onclick="location.href="contacto.html"">Contáctanos</button>
+                        <!--a href="contacto.html"><p class="footSubject">Contáctanos</p></a-->
                     </div>
                     <div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3"></div>
                 </div>
