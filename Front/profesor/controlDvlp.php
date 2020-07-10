4614113bfc3ca49db006ba0918e7cd8c950c52f6
brjupo <brjupo@gmail.com>
2020-07-10 16:01:41 -0500

    asd

--- Front/profesor/controlDvlp.php
+++ Front/profesor/controlDvlp.php
@@ -241,7 +241,7 @@ function printTeacherControlPanel()
       <div class="container">
         <div class="row">
           <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <a href="linksGenially/elegirAsignatura.php">
+            <a href="linksEnglishGenially/elegirAsignatura.php">
               <button type="button" class="btn btn-danger">
                 Add Genially s Link [English]
               </button>
