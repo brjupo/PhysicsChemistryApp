9c0a7a93ced92eb74dc51f1c173c90116f2a21ce
cmendezv <cmendez222@gmail.com>
2020-07-11 01:30:02 -0500

    sumasssd

--- Servicios/subirUsuarios.php
+++ Servicios/subirUsuarios.php
@@ -32,7 +32,7 @@ if (isset($_POST["Import"])) {
     while (($getData = fgetcsv($file, 10000, ",")) !== FALSE) {
       
       $mailr = $getData[0];
-      $compy = 'correo';
+      $compy = 'correos';
       $vatemp = $mailr + $compy;
 
       if($mailr != $compy){
