df390f75694e37e31db8124a046560d603c80e9b
brjupo <brjupo@gmail.com>
2020-07-12 18:28:29 -0500

    asd

--- Front/preguntas/practice.php
+++ Front/preguntas/practice.php
@@ -512,7 +512,7 @@
                     <div class="row">
                         <!--div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3"></div-->
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                            <button id="' . $IDBotonAceptar . '" class="miniBoton">Acepto</button>
+                            <button id="' . $IDBotonAceptar . '" class="miniBoton">Ok</button>
                             <p id="' . $IDvalorCorrecto . '" style="display:none">
                             ' . $respCorrecta . '
                             </p>
