2cff5fa22687bc733f91a14470fb9a44ef89c8a0
brjupo <brjupo@gmail.com>
2020-07-16 20:11:22 -0500

    asd

--- Front/preguntas/Sprint/subirPuntosByType.php
+++ Front/preguntas/Sprint/subirPuntosByType.php
@@ -7,35 +7,33 @@ $puntosNuevos = $_POST["puntos"];
 $flagTipo = $_POST["flagTipo"];
 
 //Lanzar consulta para saber si existe calificacion y la trae
-$statement = mysqli_prepare($con, "SELECT puntuacion FROM puntuacion WHERE id_leccion = ? AND id_usuario = ? AND tipo = ?");
-mysqli_stmt_bind_param($statement, "sss", $leccion, $id_usuario, $flagTipo);
-mysqli_stmt_execute($statement);
-mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $puntuacion);
+$puntosActuales = 0;
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stringQuery = "SELECT puntuacion FROM puntuacion WHERE id_leccion = '" . $leccion . "' AND id_usuario = '" . $id_usuario . "' AND tipo = '" . $flagTipo . "' LIMIT 1;";
+  $stmt = $conn->query($stringQuery);
+  while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+    //Leemos la calificacion 
+    $puntosActuales = $row[0];
+  }
+} catch (PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
 
-$puntosActuales = 'xxx';
 
 $response["response"] = 'failed';
-//Leemos la calificacion 
-while (mysqli_stmt_fetch($statement)) { //si si existe 
-  $puntosActuales = $puntuacion;
-}
-
-//print_r($puntosActuales);
-
-if ($puntosActuales != 'xxx' OR $puntosActuales == NULL) { //validamos que exista una calificacion $puntosActuales != NULL or $puntosActuales == 0
-  if ($puntosNuevos > $puntosActuales) {
+if (is_null($puntosActuales) || $puntosActuales==0) {
+  $sql = "INSERT INTO puntuacion(id_usuario, id_leccion, puntuacion, tipo) VALUES ($id_usuario, $leccion, $puntosNuevos,'$flagTipo')";
+  mysqli_query($con, $sql);
+  $response["response"] = 'exito';
+} else {
+  if ($puntosNuevos >= $puntosActuales) {
     //Lanzar consulta para actualizar calificacion solo si es mayor
     $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
     mysqli_query($con, $sql);
     $response["response"] = 'exito';
   }
-} else {
-  $sql = "INSERT INTO puntuacion(id_usuario, id_leccion, puntuacion, tipo) VALUES ('$id_usuario', '$leccion', '$puntosNuevos','$flagTipo')";
-  mysqli_query($con, $sql);
-  $response["response"] = 'exito';
 }
-
 echo json_encode($response);
-
-?>
