c791c748756baf8870114f0c1d62dd8eebe01b96
brjupo <brjupo@gmail.com>
2020-07-16 20:02:23 -0500

    asd

--- Front/preguntas/Sprint/subirPuntosByType.php
+++ Front/preguntas/Sprint/subirPuntosByType.php
@@ -13,22 +13,29 @@ mysqli_stmt_execute($statement);
 mysqli_stmt_store_result($statement);
 mysqli_stmt_bind_result($statement, $puntuacion);
 
+$puntosActuales = 'xxx';
+
+$response["response"] = 'failesasdfsd';
 //Leemos la calificacion 
 while (mysqli_stmt_fetch($statement)) { //si si existe 
   $puntosActuales = $puntuacion;
 }
-$response["response"] = 'failed';
-if (is_null($puntosActuales)){
-  $sql = "INSERT INTO puntuacion(id_usuario, id_leccion, puntuacion, tipo) VALUES ($id_usuario, $leccion, $puntosNuevos,'$flagTipo')";
-  mysqli_query($con, $sql);
-  $response["response"] = 'exito';
-}
-else{ 
-  if ($puntosNuevos >= $puntosActuales) {
+
+//print_r($puntosActuales);
+
+if ($puntosActuales != 'xxx' OR $puntosActuales == NULL) { //validamos que exista una calificacion $puntosActuales != NULL or $puntosActuales == 0
+  if ($puntosNuevos > $puntosActuales) {
     //Lanzar consulta para actualizar calificacion solo si es mayor
     $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
     mysqli_query($con, $sql);
-    $response["response"] = 'exito';
+    $response["response"] = 'exito1ul';
   }
+} else {
+  $sql = "INSERT INTO puntuacion(id_usuario, id_leccion, puntuacion, tipo) VALUES ('$id_usuario', '$leccion', '$puntosNuevos','$flagTipo')";
+  mysqli_query($con, $sql);
+  $response["response"] = 'exito2';
 }
+
 echo json_encode($response);
+
+?>
