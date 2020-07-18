53ecea765626fb038f499917be78e409c60505c2
brjupo <brjupo@gmail.com>
2020-07-18 09:42:40 -0500

    asd

--- Front/preguntas/Sprint/subirPuntosByType.php
+++ Front/preguntas/Sprint/subirPuntosByType.php
@@ -18,7 +18,6 @@ try {
   $stmt = $conn->query($stringQuery);
   while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
     //Leemos la calificacion 
-    //echo  $row[0];
     $puntosActuales = $row[0];
   }
 } catch (PDOException $e) {
@@ -26,21 +25,19 @@ try {
 }
 $conn = null;
 
-//echo $puntosActuales;
-//echo $puntosNuevos;
 $response["response"] = 'failed';
 if (is_null($puntosActuales)) {
   $sql = "INSERT INTO puntuacion(id_usuario, id_leccion, puntuacion, tipo) VALUES ($id_usuario, $leccion, $puntosNuevos,'$flagTipo')";
   mysqli_query($con, $sql);
-  $response["response"] = 'exitoDeberiaSerNUll';
+  $response["response"] = 'exito';
 } 
 else if ($puntosNuevos > $puntosActuales) {
     //Lanzar consulta para actualizar calificacion solo si es mayor
     $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
     mysqli_query($con, $sql);
-    //$response["response"] = 'exito'; 
+    $response["response"] = 'exito'; 
 }
 else if($puntosNuevos <= $puntosActuales){
-  //$response["response"] = 'exito'; 
+  $response["response"] = 'exito'; 
 }
 echo json_encode($response);
