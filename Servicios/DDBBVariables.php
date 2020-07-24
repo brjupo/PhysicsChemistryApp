fa493d97c71f78fda8ed3392fd1666eb60b7952a
brjupo <brjupo@gmail.com>
2020-07-24 12:03:25 -0500

    asd

--- Servicios/DDBBVariables.php
+++ Servicios/DDBBVariables.php
@@ -1,4 +1,6 @@
 <?php
+//NO BORRAR, varios archivos dependen de estan variables
+//Si lo borran, esas vistas no funcionaran o estaran llenas de errores
 //Informacion de la BBDD
 $servername = "localhost";
 $username = "u526597556_dev";
