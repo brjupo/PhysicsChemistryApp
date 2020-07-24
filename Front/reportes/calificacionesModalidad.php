3901f592d6ab100e4b953ddc97f16f0dff2404d4
brjupo <brjupo@gmail.com>
2020-07-24 13:15:54 -0500

    asd

--- Front/reportes/calificacionesModalidad.php
+++ Front/reportes/calificacionesModalidad.php
@@ -117,7 +117,7 @@ require '../../Servicios/DDBBVariables.php';
     <div class="container">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <table>
+                <table style="border-collapse: separate !important; white-space: nowrap !important;">
                     <tbody>
                         <tr class="table-success">
                             <td style="color:white;">.</td>
