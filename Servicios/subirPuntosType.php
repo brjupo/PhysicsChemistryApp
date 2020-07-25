d731e574c75fd2b2e49b0128f25ed64a4ac42884
Cristian <cmendez222@gmail.com>
2020-07-24 23:38:35 -0500

    tex

--- Servicios/subirPuntosType.php
+++ Servicios/subirPuntosType.php
@@ -38,7 +38,7 @@ while (mysqli_stmt_fetch($statement)) { //si si existe
 if (1) { //$puntosNuevos > $puntosActuales validamos que exista una calificacion $puntosActuales != NULL or $puntosActuales == 0  $puntosActuales != 'xxx' OR $puntosActuales == NULL
   if (1) {
     //Lanzar consulta para actualizar calificacion solo si es mayor
-    $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos, tiempo = $tiempo WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
+    $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos, tiempo = '$tiempo' WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
     mysqli_query($con, $sql);
   }
   $response["response"] = 'exito';
