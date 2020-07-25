a7fbf9bd2f627692ae1ffeee431f7a10f23bf3f1
brjupo <brjupo@gmail.com>
2020-07-25 18:38:53 -0500

    asd

--- Front/reportes/elegirTSL/oneGroupOneMode.php
+++ Front/reportes/elegirTSL/oneGroupOneMode.php
@@ -76,16 +76,15 @@ function printTitle()
 function printCabecera()
 {
     echo '
-
     <!--+++++++++++++++++++++++++++++++++++ CABECERA [Asignatura, Profesor, Grupo y Modalidad] +++++++++++++++++++++++++++++++++++++-->
-    <p> INPUTS: Id de grupo y Modalidad</p>
     ';
     global $servername, $username, $password, $dbname;
     //Crear la lectura en base de datos
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stringQuery = "SELECT DISTINCT a.nombre, u.mail, g.nombre, pu.tipo FROM asignatura a JOIN grupo g JOIN profesor prof JOIN usuario_prueba u JOIN puntuacion pu ON g.id_asignatura = a.id_asignatura AND g.id_profesor = prof.id_profesor AND prof.id_usuario = u.id_usuario WHERE g.id_grupo = " . $_GET["grupo"] . " AND pu.tipo = '" . $_GET["modo"] . "';";
+        $stringQuery = "SELECT DISTINCT a.nombre, u.mail, g.nombre, pu.nombre FROM asignatura a JOIN grupo g JOIN profesor prof JOIN usuario_prueba u JOIN puntuacion pu ON g.id_asignatura = a.id_asignatura AND g.id_profesor = prof.id_profesor AND prof.id_usuario = u.id_usuario WHERE g.id_grupo = " . $_GET["grupo"] . " AND pu.tipo = '" . $_GET["modo"] . "';";
+        //$stringQuery = "SELECT DISTINCT a.nombre, u.mail, g.nombre, pu.tipo FROM asignatura a JOIN grupo g JOIN profesor prof JOIN usuario_prueba u JOIN puntuacion pu ON g.id_asignatura = a.id_asignatura AND g.id_profesor = prof.id_profesor AND prof.id_usuario = u.id_usuario WHERE g.id_grupo = " . $_GET["grupo"] . " AND pu.tipo = '" . $_GET["modo"] . "';";
         $stmt = $conn->query($stringQuery);
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
             //row [0] -> Materia, mail, grupo, modalidad
