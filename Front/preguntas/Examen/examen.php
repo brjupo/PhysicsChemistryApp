00f923f8feec855d550b0d1e0fabd0e8c7380c3d
Cristian <cmendez222@gmail.com>
2020-07-25 00:36:08 -0500

    slv

--- Front/preguntas/Examen/examen.php
+++ Front/preguntas/Examen/examen.php
@@ -14,7 +14,6 @@
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 </head>
 
-
 <body>
     <script>
         document.addEventListener("contextmenu", (event) => event.preventDefault());
