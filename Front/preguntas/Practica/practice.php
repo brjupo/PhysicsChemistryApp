b6ef32f4cdc33f236ea2fa1a5e3536d18fbd1b86
Cristian <cmendez222@gmail.com>
2020-07-24 22:17:07 -0500

    script

--- Front/preguntas/Practica/practice.php
+++ Front/preguntas/Practica/practice.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="../../CSSsJSs/bootstrap341.css" />
     <link rel="stylesheet" href="../../CSSsJSs/stylePreguntas13.css" />
     <script src="../../CSSsJSs/scriptPractice1.js"></script>
-    <script src="../../CSSsJSs/minAJAX.js"></script>
+    <script src="../../preguntas/Practica/scriptPractice1.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 </head>
