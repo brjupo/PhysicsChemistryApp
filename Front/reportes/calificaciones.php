f715f3ca7514ba60bda9b80532821af6cba35c70
brjupo <brjupo@gmail.com>
2020-07-26 13:56:58 -0500

    asd

---
+++ Front/reportes/calificaciones.php
@@ -0,0 +1,114 @@
+<?php
+require '../../Servicios/DDBBVariables.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" type="image/x-icon" href="../CSSsJSs/icons/pyramid.svg" />
+    <title>Kaanbal</title>
+    <link rel="stylesheet" href="../CSSsJSs/bootstrap441.css" />
+    <link rel="stylesheet" href="../CSSsJSs/kaanbalEssentials.css" />
+</head>
+
+<body>
+    <style>
+        table {
+            border-collapse: separate !important;
+            white-space: nowrap !important;
+        }
+
+        td {
+            text-align: center;
+        }
+    </style>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"></div>
+            <div class="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11"></div>
+            <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10"></div>
+            <div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9"></div>
+            <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8"></div>
+            <div class="col-7 col-sm-7 col-md-7 col-lg-7 col-xl-7"></div>
+            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6"></div>
+            <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
+            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+            <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3"></div>
+            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+            <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+        </div>
+    </div>
+    <!--+++++++++++++++++++++++++++++++++++ Logo Kaanbal ++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
+    <div class="container">
+        <div class="row">
+            <div class="textCenter col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+            <div class="textLeft col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                <p class="titulo">Kaanbal</p>
+            </div>
+            <div class="textCenter col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6"></div>
+        </div>
+    </div>
+
+    <!--+++++++++++++++++++++++++++++++++++ CABECERA [Asignatura, Profesor, Grupo y Modalidad] +++++++++++++++++++++++++++++++++++++-->
+    <p> INPUTS: Id de grupo y Modalidad</p>
+    <?php
+    //Crear la lectura en base de datos
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stringQuery = "SELECT DISTINCT a.nombre, u.mail, g.nombre, pu.tipo FROM asignatura a JOIN grupo g JOIN profesor prof JOIN usuario_prueba u JOIN puntuacion pu ON g.id_asignatura = a.id_asignatura AND g.id_profesor = prof.id_profesor AND prof.id_usuario = u.id_usuario WHERE g.id_grupo = 1 AND pu.tipo = 'SP';";
+        $stmt = $conn->query($stringQuery);
+        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+            //row [0] -> Materia, mail, grupo, modalidad
+            $materia = $row[0];
+            $correoProfesor = $row[1];
+            $grupo = $row[2];
+            $modalidad = $row[3];
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                <table style="width:100%">
+                    <tbody>
+                        <tr class="table-info">
+                            <td>Asignatura</td>
+                            <td><?php echo $materia; ?></td>
+                        </tr>
+                        <tr class="table-light">
+                            <td>Profesor</td>
+                            <td><?php echo $correoProfesor; ?></td>
+                        </tr>
+                        <tr class="table-info">
+                            <td>Grupo</td>
+                            <td><?php echo $grupo; ?></td>
+                        </tr>
+                        <tr class="table-light">
+                            <td>Modalidad</td>
+                            <td><?php echo $modalidad; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+        </div>
+    </div>
+
+    <!--+++++++++++++++++++++++++++++++++++ Temas, Subtemas y Lecciones +++++++++++++++++++++++++++++++++++++-->
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"></div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
