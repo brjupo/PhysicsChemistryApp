3cddcb261fd725831765a5ff41b2cb30d0c5d154
brjupo <brjupo@gmail.com>
2020-07-26 22:09:40 -0500

    asd

--- Front/reportes/elegirTSL/elegirMyG.php
+++ Front/reportes/elegirTSL/elegirMyG.php
@@ -2,52 +2,27 @@
 require "../../../Servicios/DDBBVariables.php";
 require "../../../Servicios/isTeacher.php";
 $teacherID = isTeacher();
-if ($teacherID=="null") {
-    header('Location: https://kaanbal.net/');
-    exit;
-} else {
-    printMYG($teacherID);
+if ($teacherID == "null") {
+  header('Location: https://kaanbal.net/');
+  exit;
 }
+?>
 
+<!DOCTYPE html>
+<html>
 
-function printMyG($teacherID)
-{
-    echo '
-        <!DOCTYPE html>
-        <html>';
-    printHead();
-    echo '<body>';
-    printTitle();
-    printInstructions();
-    printCombos($teacherID);
-    printButtons();
-    echo '
-        </body>
-        </html>';
-}
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" type="image/x-icon" href="../../CSSsJSs/icons/pyramid.svg" />
+  <title>Kaanbal</title>
+  <link rel="stylesheet" href="../../CSSsJSs/bootstrap441.css" />
+  <link rel="stylesheet" href="../../CSSsJSs/kaanbalEssentials10.css" />
+  <script src="elegirMyG3.js"></script>
+</head>
 
+<body>
 
-function printHead()
-{
-    echo '
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link
-      rel="shortcut icon"
-      type="image/x-icon"
-      href="../../CSSsJSs/icons/pyramid.svg"
-    />
-    <title>Kaanbal</title>
-    <link rel="stylesheet" href="../../CSSsJSs/bootstrap441.css" />
-    <link rel="stylesheet" href="../../CSSsJSs/kaanbalEssentials10.css" />
-    <script src="elegirMyG3.js"></script>
-  </head>
-  ';
-}
-function printTitle()
-{
-    echo '
   <div class="container">
     <div class="row">
       <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
@@ -66,19 +41,13 @@ function printTitle()
       </div>
     </div>
   </div>
-  ';
-}
-
-function printInstructions()
-{
-    echo '
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <p>
           - Elija el grupo y la modalidad, y de clic en "generar reporte".
         </p>
-        <p id="leccion">'.$_GET["ID_Leccion"].'</p>
+        <p id="leccion"><?php echo $_GET["ID_Leccion"]; ?></p>
       </div>
     </div>
   </div>
@@ -90,66 +59,51 @@ function printInstructions()
       </div>
     </div>
   </div>
-  ';
-}
-
-function printCombos($teacherID)
-{
-    echo '
   <div class="container">
     <div class="row">
       <div class="input-group col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <div class="input-group-prepend">
-            <label class="input-group-text" for="grupo">Grupo</label>
-            <p>'.$teacherID.'</p>
+          <label class="input-group-text" for="grupo">Grupo</label>
         </div>
-            <select class="custom-select" id="grupo">
-                <option selected disabled value="0">Elegir</option>';
+        <select class="custom-select" id="grupo" name="grupo" form="groupForm">
+          <option selected disabled value="0">Elegir</option>';
+          <?php
+          global $servername, $username, $password, $dbname;
+          //Crear la lectura en base de datos
+          try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stringQuery = 'SELECT id_grupo, nombre FROM grupo WHERE id_profesor = ' . $teacherID . ';';
+            $stmt = $conn->query($stringQuery);
+            while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+              echo '<option value="' . $row[0] . '">' . $row[1] . '</option>';
+            }
+          } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+          }
+          $conn = null;
 
-    global $servername, $username, $password, $dbname;
-    //Crear la lectura en base de datos
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stringQuery = 'SELECT id_grupo, nombre FROM grupo WHERE id_profesor = ' . $teacherID . ';';
-        $stmt = $conn->query($stringQuery);
-        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            echo '<option value="' . $row[0] . '">' . $row[1] . '</option>';
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-
-    echo '
-            </select>
+          ?>
+        </select>
       </div>
-    </div>
-  </div>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p style="color: rgba(0, 0, 0, 0);">.</p>
-      </div>
-    </div>
-  </div>
-  ';
-
-    echo '
-  <div class="container">
-    <div class="row">
       <div class="input-group col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <div class="input-group-prepend">
-            <label class="input-group-text" for="modalidad">Modalidad</label>
+          <label class="input-group-text" for="modalidad">Modalidad</label>
         </div>
-            <select class="custom-select" id="modalidad">
-                <option selected disabled value="0">Elegir</option>
-                <option value="PP">Práctica</option>
-                <option value="SP">Sprint</option>
-                <option value="E">Examen</option>
-                <option value="SS">Super Sprint</option>
-            </select>
+        <select class="custom-select" id="modalidad" name="modalidad" form="groupForm">
+          <option selected disabled value="0">Elegir</option>
+          <option value="PP">Práctica</option>
+          <option value="SP">Sprint</option>
+          <option value="E">Examen</option>
+          <!--option value="SS">Super Sprint</option-->
+        </select>
+      </div>
+
+      <div class="input-group col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <form action="oneGroupOneMode.php" id="groupForm" method="POST">
+          <input type="submit" class="btn btn-primary btn-sm" value="Generar reporte"><br>
+        </form>
       </div>
     </div>
   </div>
@@ -161,32 +115,3 @@ function printCombos($teacherID)
       </div>
     </div>
   </div>
-  ';
-}
- function printButtons(){
-    echo '
-    <div class="container">
-      <div class="row">
-        <div
-          class="input-group input-group-sm col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6"
-        >
-          <button
-            id="generateReport"
-            type="button"
-            class="btn btn-primary btn-sm"
-          >
-          Generar reporte
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-          <p style="color: rgba(0, 0, 0, 0);">.</p>
-        </div>
-      </div>
-    </div>
-    ';
-  }
-  
--- Front/reportes/elegirTSL/oneGroupOneMode.php
+++ Front/reportes/elegirTSL/oneGroupOneMode.php
@@ -5,79 +5,62 @@ $teacherID = isTeacher();
 if ($teacherID == "null") {
     header('Location: https://kaanbal.net/');
     exit;
-} else {
-    print1g1m();
 }
-
-function print1g1m()
-{
-    echo '
-    <!DOCTYPE html>
-    <html>';
-    printHead();
-    echo '<body>';
-    printTitle();
-    printCabecera();
-    printTSL();
-    echo '
-    </body>
-    </html>';
+if (!isset($_POST["grupo"]) && !isset($_POST["modalidad"])) {
+    echo '<p>';
+    echo $_POST["grupo"];
+    echo '<br>';
+    echo $_POST["modalidad"];
+    echo '</p>';
 }
+?>
+<!DOCTYPE html>
+<html>
 
-function printHead()
-{
-    echo '
-  <head>
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link
-      rel="shortcut icon"
-      type="image/x-icon"
-      href="../../CSSsJSs/icons/pyramid.svg"
-    />
+    <link rel="shortcut icon" type="image/x-icon" href="../../CSSsJSs/icons/pyramid.svg" />
     <title>Kaanbal</title>
     <link rel="stylesheet" href="../../CSSsJSs/bootstrap441.css" />
     <link rel="stylesheet" href="../../CSSsJSs/kaanbalEssentials10.css" />
-  </head>
-  ';
-}
-function printTitle()
-{
-    echo '
+</head>
+
+<body>
+    <?php
+    $id_grupo = $_POST["grupo"];
+    $tipo = $_POST["modalidad"];
+    ?>
     <style>
         table {
-            border-collapse: separate !important; white-space: nowrap !important;
+            border-collapse: separate !important;
+            white-space: nowrap !important;
         }
+
         td {
             text-align: center;
         }
     </style>
     <div class="container">
         <div class="row">
-        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-        <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
-            <h1 class="titulo">Kaanbal</h1>
-        </div>
-        <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
-        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+            <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+            <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                <h1 class="titulo">Kaanbal</h1>
+            </div>
+            <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
+            <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
         </div>
     </div>
 
     <div class="container">
         <div class="row">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-        </div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <p style="color: rgba(0, 0, 0, 0);">.</p>
+            </div>
         </div>
     </div>
-  ';
-}
-
-function printCabecera()
-{
-    echo '
     <!--+++++++++++++++++++++++++++++++++++ CABECERA [Asignatura, Profesor, Grupo y Modalidad] +++++++++++++++++++++++++++++++++++++-->
-    ';
+    <?php
     global $servername, $username, $password, $dbname;
     //Crear la lectura en base de datos
     try {
@@ -97,11 +80,11 @@ function printCabecera()
     }
     $conn = null;
     // (Condition)?(thing's to do if condition true):(thing's to do if condition false);
-    if ($modalidad=="PP")$modalidad="Práctica";
-    if ($modalidad=="SP")$modalidad="Sprint";
-    if ($modalidad=="E")$modalidad="Examen";
-    if ($modalidad=="SS")$modalidad="Super Sprint";
-    echo '
+    if ($modalidad == "PP") $modalidad = "Práctica";
+    if ($modalidad == "SP") $modalidad = "Sprint";
+    if ($modalidad == "E") $modalidad = "Examen";
+    if ($modalidad == "SS") $modalidad = "Super Sprint";
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
@@ -109,19 +92,19 @@ function printCabecera()
                     <tbody>
                         <tr>
                             <td>Asignatura</td>
-                            <td>' . $materia . '</td>
+                            <td><?php echo $materia; ?></td>
                         </tr>
                         <tr class="table-light">
                             <td>Profesor</td>
-                            <td>' . $correoProfesor . '</td>
+                            <td><?php echo $correoProfesor; ?></td>
                         </tr>
                         <tr>
                             <td>Grupo</td>
-                            <td>' . $grupo . '</td>
+                            <td><?php echo $grupo; ?></td>
                         </tr>
                         <tr class="table-light">
                             <td>Modalidad</td>
-                            <td>' . $modalidad . '</td>
+                            <td><?php echo $modalidad; ?></td>
                         </tr>
                     </tbody>
                 </table>
@@ -129,11 +112,7 @@ function printCabecera()
             <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
         </div>
     </div>
-    ';
-}
-
-function printTSL()
-{
+    <?php
     global $servername, $username, $password, $dbname;
     //Crear la lectura en base de datos
     //SELECT nombre FROM tema WHERE id_tema IN (SELECT id_tema FROM subtema WHERE id_subtema IN (SELECT id_subtema FROM leccion WHERE id_leccion = 151)) 
@@ -174,7 +153,7 @@ function printTSL()
         echo "Error: " . $e->getMessage();
     }
     $conn = null;
-    echo '
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -191,30 +170,32 @@ function printTSL()
                         <tr class="table-info">
                             <td>.</td>
                             <td>' . $leccion . '</td>
-                        </tr>';
-    //Crear la lectura en base de datos
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stringQuery = "SELECT a.matricula, SUM(p.puntuacion) AS 'diamantes' FROM puntuacion p JOIN usuario_prueba u JOIN alumno a ON p.id_usuario = u.id_usuario AND u.id_usuario = a.id_usuario WHERE a.id_alumno IN (SELECT id_alumno FROM alumno_grupo WHERE id_grupo = 1) GROUP BY a.matricula ORDER BY matricula ASC;";
-        $stmt = $conn->query($stringQuery);
-        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            //row [0] -> matricula, diamantes
-            echo '
-            <tr class="table-light">
-                <td>' . $row[0] . '</td>
-                <td></td>
-            </tr>';
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-    echo '
+                        </tr>
+                        <?php
+                        //Crear la lectura en base de datos
+                        try {
+                            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                            $stringQuery = "SELECT a.matricula, SUM(p.puntuacion) AS 'diamantes' FROM puntuacion p JOIN usuario_prueba u JOIN alumno a ON p.id_usuario = u.id_usuario AND u.id_usuario = a.id_usuario WHERE a.id_alumno IN (SELECT id_alumno FROM alumno_grupo WHERE id_grupo = 1) GROUP BY a.matricula ORDER BY matricula ASC;";
+                            $stmt = $conn->query($stringQuery);
+                            while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+                                //row [0] -> matricula, diamantes
+                                echo '
+                                <tr class="table-light">
+                                    <td>' . $row[0] . '</td>
+                                    <td></td>
+                                </tr>';
+                            }
+                        } catch (PDOException $e) {
+                            echo "Error: " . $e->getMessage();
+                        }
+                        $conn = null;
+                        ?>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-    ';
-}
+</body>
+
+</html>
