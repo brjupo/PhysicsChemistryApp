d0cb621412043c6dbaf6e6351983d4a553494488
Cristian <cmendez222@gmail.com>
2020-07-29 23:11:57 -0500

    vengaaa

--- Front/preguntas/Practica/scriptPractice9.js
+++ Front/preguntas/Practica/scriptPractice9.js
@@ -14,6 +14,7 @@ window.onload = function () {
   createArrayWithQuestions();
   contarTiempo();
   idioma = document.getElementById("idioma").innerHTML.trim();
+  registrarInactividad();
 };
 
 function contarTiempo() {
@@ -649,3 +650,28 @@ $.ajax({
   },
 });
 }
+
+var registrarInactividad = function () {
+  var t;
+  window.onload = reiniciarTiempo;
+  // Eventos del DOM
+  document.onmousemove = reiniciarTiempo;
+  document.onkeypress = reiniciarTiempo;
+  document.onload = reiniciarTiempo;
+  document.onmousemove = reiniciarTiempo;
+  document.onmousedown = reiniciarTiempo; // aplica para una pantalla touch
+  document.ontouchstart = reiniciarTiempo;
+  document.onclick = reiniciarTiempo;     // aplica para un clic del touchpad
+  document.onscroll = reiniciarTiempo;    // navegando con flechas del teclado
+  document.onkeypress = reiniciarTiempo;
+
+  function tiempoExcedido() {
+      alert("Estuvo inactivo durante mucho tiempo.")
+  }
+
+  function reiniciarTiempo() {
+      clearTimeout(t);
+      t = setTimeout(tiempoExcedido, 3000)
+      // 1000 milisegundos = 1 segundo
+  }
+};
