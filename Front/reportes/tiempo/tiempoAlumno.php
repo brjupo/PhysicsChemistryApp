3313b689c96bfb72f75f8e6d3b16cbb40f94ac64
brjupo <brjupo@gmail.com>
2020-08-02 14:30:10 -0500

    con total

--- Front/reportes/tiempo/tiempoAlumno.php
+++ Front/reportes/tiempo/tiempoAlumno.php
@@ -154,7 +154,8 @@ if (!isset($_POST["grupo"])) {
                             while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
                                 echo '<tr>';
                                 echo '<td>'.$row[0].'</td>';
-                                echo '<td>'.$row[1]+$row[2]+$row[3]+$row[4].'</td>';
+                                $sum = $row[1]+$row[2]+$row[3]+$row[4];
+                                echo '<td>'.$sum.'</td>';
                                 echo '<td>'.$row[1].'</td>';
                                 echo '<td>'.$row[2].'</td>';
                                 echo '<td>'.$row[3].'</td>';
