32c13f9abd6a0e72d7d72bf1efb9b2eba63d1eaf
brjupo <brjupo@gmail.com>
2020-08-19 21:34:52 -0500

    asd

--- Front/staff/controlStaff.php
+++ Front/staff/controlStaff.php
@@ -3,132 +3,148 @@ require "../../Servicios/DDBBVariables.php";
 require "../../Servicios/isStaff.php";
 $staffID = isStaff();
 if ($staffID == "null") {
-    header('Location: https://kaanbal.net/');
-    exit;
+  header('Location: https://kaanbal.net/');
+  exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link
-        rel="shortcut icon"
-        type="image/x-icon"
-        href="../CSSsJSs/icons/pyramid.svg"
-      />
-      <title>Kaanbal</title>
-      <link rel="stylesheet" href="../CSSsJSs/bootstrap441.css" />
-      <link rel="stylesheet" href="../CSSsJSs/kaanbalEssentials10.css" />
-    </head>
-  
-    <body>
-    
-      <div class="container">
-        <div class="row">
-          <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-          <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
-            <h1 class="titulo">Kaanbal</h1>
-          </div>
-          <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
-          <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <h4>Staff</h4>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <a href="createGroup.php">
-              <button type="button" class="btn btn-info">
-                Crear grupos
-              </button>
-            </a>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-          </div>
-        </div>
-      </div>
-      
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <a href="uploadAlumniGroup.php">
-              <button type="button" class="btn btn-secondary">
-                Subir archivo CSV de alumnos[o profesores] + password + grupo
-              </button>
-            </a>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-          </div>
-        </div>
-      </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" type="image/x-icon" href="../CSSsJSs/icons/pyramid.svg" />
+  <title>Kaanbal</title>
+  <link rel="stylesheet" href="../CSSsJSs/bootstrap441.css" />
+  <link rel="stylesheet" href="../CSSsJSs/kaanbalEssentials10.css" />
+</head>
 
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <a href="showAllAlumni.php">
-              <button type="button" class="btn btn-success">
-                Ver todos los alumnos y su grupo
-              </button>
-            </a>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-            <p style="color: rgba(0, 0, 0, 0);">.</p>
-          </div>
-        </div>
-      </div>
+<body>
 
-    </body>
+  <div class="container">
+    <div class="row">
+      <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+      <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+        <h1 class="titulo">Kaanbal</h1>
+      </div>
+      <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
+      <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <h4>Staff</h4>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <a href="createGroup.php">
+          <button type="button" class="btn btn-info">
+            Crear grupos
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <a href="uploadAlumniGroup.php">
+          <button type="button" class="btn btn-secondary">
+            Subir archivo CSV de alumnos[o profesores] + password + grupo
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <a href="showAllAlumni.php">
+          <button type="button" class="btn btn-success">
+            Ver todos los alumnos y su grupo
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <a href="enviarLink.php">
+          <button type="button" class="btn btn-info">
+            PAGADO de licencias
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+</body>
 
 </html>
