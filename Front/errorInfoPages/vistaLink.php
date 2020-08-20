3f8de2a3f25d7af2dcd54b25de579982f2ec161a
Cristian <cmendez222@gmail.com>
2020-08-19 20:18:15 -0500

    vengasfdf

--- Front/errorInfoPages/vistaLink.php
+++ Front/errorInfoPages/vistaLink.php
@@ -30,20 +30,16 @@
     <div class="row">
       <div class="textCenter col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
       <div class="textCenter col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
-        <form oninput='psw2.setCustomValidity(psw2.value != psw.value ? "\n Passwords do not match." : "")'>
-          <?php
-          //echo "<p>" . $tokenLink . "  " . $correo_e . "   ";
-          ?>
           <input type="text" id="token" value="<?php echo $tokenLink ?>" style="display:none;">
 
-          <label for="correo_e">Correo</label>
-          <input type="email" id="correo_e" name="correo_e" value="<?php echo $correo_e ?>" disabled />
+          <label for="correo_e">Usuario</label>
+          <input type="email" id="correo_e" name="correo_e">
 
           <label for="psw">Password</label>
-          <input type="password" id="psw" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+          <input type="password" id="psw" name="psw"/>
 
-          <label for="password2">Confirm Your Password</label>
-          <input type="password" id="psw2" name="psw2" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+          <label for="password2">Alumno Kaanbal</label>
+          <input type="password" id="psw2" name="psw2" />
         </form>
       </div>
       <div class="textCenter col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
