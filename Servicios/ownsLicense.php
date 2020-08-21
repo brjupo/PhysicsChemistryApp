318f7e43c28fd559be62a32a6c70319b072c5034
brjupo <brjupo@gmail.com>
2020-08-20 23:04:54 -0500

    asdasd

--- Servicios/ownsLicense.php
+++ Servicios/ownsLicense.php
@@ -5,23 +5,27 @@ require "DDBBVariables.php";
 //////////////////////////////////////////////////////
 
 $pila = array();
+//$idAsignatura = $_SESSION["idAsignatura"];
+
 
 function ownsLicense()
-{   
+{
     global $servername, $username, $password, $dbname;
 
-
+    $mail = $_SESSION["mail"];
     //Crear la lectura en base de datos
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stringQuery="SELECT";
+        $stringQuery = "SELECT licencia.id_asignatura 
+        FROM licencia 
+        INNER JOIN usuario_prueba ON licencia.id_usuario = usuario_prueba.id_usuario 
+        WHERE usuario_prueba.mail = '" . $mail . "' ";
         $stmt = $conn->query($stringQuery);
-        while ($row = $stmt->fetch(PDO::FETCH_NUM)){
+        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
             array_push($pila, $row[0]);
-        }															
-    }
-    catch(PDOException $e) {
+        }
+    } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
     $conn = null;
