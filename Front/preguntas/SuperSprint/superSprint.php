9fd9d61d0614e3ad459f8e7d404e5f6d28e67741
Cristian <cmendez222@gmail.com>
2020-08-21 12:43:52 -0500

    venga

--- Front/preguntas/SuperSprint/superSprint.php
+++ Front/preguntas/SuperSprint/superSprint.php
@@ -90,6 +90,54 @@
         $idSubtema = $arregloIdleccion["id_subtema"];-------CAMBIADO POR EL BRANDON A LAS 18:00 EL 2 DE JUNIO
         */
         $idSubtema = $subtema;
+
+        //Validar licencia
+        /////////id_lecciones traer id asignaturas licencias
+        $statement = mysqli_prepare($con, "SELECT id_asignatura FROM licencia WHERE id_usuario = ?");
+        mysqli_stmt_bind_param($statement, "s", $_SESSION["id_usuario"]);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        mysqli_stmt_bind_result($statement, $id_asignatura);
+    
+        $arregloAsignaturas = array();
+    
+        $i = 0;
+        while (mysqli_stmt_fetch($statement)) {
+        $arregloAsignaturas[$i]["id_asignatura"] = $id_asignatura;
+        $i = $i + 1;
+        }
+    
+        $tamanhoArray = count($arregloAsignaturas);
+    
+        /////////id_subtema trae id_tema
+        $query = "SELECT id_tema FROM subtema WHERE id_subtema = $idSubtema";
+        $result = mysqli_query($con, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tema[] = $row;
+        }
+        $idTema = $tema[0]["id_tema"]; 
+    
+        /////////id_tema tra id_asignatura
+        $query = "SELECT id_asignatura FROM tema WHERE id_tema = $idTema";
+        $result = mysqli_query($con, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $asignatura[] = $row;
+        }
+        $idAsignatura = $asignatura[0]["id_asignatura"];
+    
+        $flag = 0;
+    
+        //for ($j = 0; $j < $tamanhoArray; $j++){ 
+            if(in_array($idAsignatura,array_column($arregloAsignaturas, 'id_asignatura')))
+            {$flag = 1;} 
+
+            if($flag == 0){
+                echo '<script type="text/javascript">
+                alert("No cuenta con licencia para acceder a esta página");
+                window.location.href="https://kaanbal.net";
+                </script>';
+            }
+        ///////////////////////////////////TERMINA VALIDAR LICENCIA
         //Traer todas las preguntas
         $query = "SELECT * FROM pregunta WHERE id_leccion IN (SELECT id_leccion FROM leccion WHERE id_subtema = $idSubtema) ORDER BY RAND();";
         //$query = "SELECT * FROM pregunta WHERE id_subtema = $idSubtema ORDER BY RAND()"; //Revolviendo preguntas, solo para sprint y examen se usa la siguiente linea antes de llamar a imprimir preguntas
