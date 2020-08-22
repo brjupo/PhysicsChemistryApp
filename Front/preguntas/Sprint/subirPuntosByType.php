5d6fbb6878cd60febda7a8be3c832a98adaea3e5
Cristian <cmendez222@gmail.com>
2020-08-22 13:35:37 -0500

    tiempo

--- Front/preguntas/Sprint/subirPuntosByType.php
+++ Front/preguntas/Sprint/subirPuntosByType.php
@@ -62,7 +62,7 @@ if (is_null($puntosActuales)) {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos , tiempo = '$tiempo' WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
+    $sql = "UPDATE puntuacion SET puntuacion = $puntosNuevos, tiempo = '$tiempo' WHERE id_leccion = $leccion AND id_usuario = $id_usuario AND tipo = '$flagTipo'";
     // use exec() because no results are returned
     $conn->exec($sql);
     $response["response"] = 'exito';
