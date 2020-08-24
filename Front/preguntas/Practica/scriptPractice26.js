3f9580a0c075e651c7de70be1ccbac2690dd812d
Cristian <cmendez222@gmail.com>
2020-08-24 12:03:05 -0500

    parcticefixa

--- Front/preguntas/Practica/scriptPractice26.js
+++ Front/preguntas/Practica/scriptPractice26.js
@@ -422,7 +422,7 @@ function nextQuestion(lastQuestion) {
   }
   if (questionNumberArray.length == 0) {
 
-    var delayInMilliseconds = 800; //2 second
+    var delayInMilliseconds = 600; // second
     setTimeout(function() {
       //your code to be executed after 1 second
     var userID = document.getElementById("userID").innerHTML.trim();
