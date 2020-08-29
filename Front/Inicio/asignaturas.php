d0510da638a2cc6bb59a6e714cb6acdddded73b6
Cristian <cmendez222@gmail.com>
2020-08-29 00:21:41 -0500

    dsdsd

--- Front/Inicio/asignaturas.php
+++ Front/Inicio/asignaturas.php
@@ -257,7 +257,7 @@ require "../../Servicios/isStaff.php";
       //Si el usuario NO EXISTE mensaje de error y retorna a inicio
       else {
         echo '<script type="text/javascript">
-            alert("Usuario y/o contraseña incorrectos<br>Adquiere tu licencia aqui");
+            alert("Usuario y/o contraseña incorrectos -> Adquiere tu licencia aqui");
             window.location.href="https://kaanbal.net/contacto.html";
             </script>';
       }
