f507ea2e867684ec22c99851ced39e4be00da546
brjupo <brjupo@gmail.com>
2020-09-20 12:19:38 -0500

    servicio para escribir la imagen en la BBDD

---
+++ Servicios/Obsoletos/images.php
@@ -0,0 +1,68 @@
+<?php
+require "DDBBVariables.php";
+
+//Leer las variables del POST
+$inicio = $_POST['inicio'];
+$fin = $_POST['fin'];
+
+//Crear la lectura en base de datos
+//Leer los IDs de las preguntas [inicio a fin]
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stringQuery = "SELECT id_pregunta FROM pregunta WHERE id_pregunta > " . $inicio . " AND id_pregunta < " . $fin;
+    $stmt = $conn->query($stringQuery);
+    while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+        //$row[0];
+        $idPregunta = $row[0];
+        echo $idPregunta;
+        findImage($idPregunta);
+    }
+} catch (PDOException $e) {
+    echo "Error al " . $stringQuery . "<br>" . $e->getMessage();
+}
+$conn = null;
+
+
+function findImage($IDPregunta)
+{
+    $pathjpg = "../../../../IMAGENES/" . $IDPregunta . ".jpg";
+    $pathpng = "../../../../IMAGENES/" . $IDPregunta . ".png";
+    $pathJPG = "../../../../IMAGENES/" . $IDPregunta . ".JPG";
+    $pathPNG = "../../../../IMAGENES/" . $IDPregunta . ".PNG";
+    $imageName = NULL;
+    if (file_exists($pathjpg)) {
+        $imageName = $IDPregunta . ".jpg";
+    } else if (file_exists($pathJPG)) {
+        $imageName = $IDPregunta . ".JPG";
+    } else if (file_exists($pathpng)) {
+        $imageName = $IDPregunta . ".png";
+    } else if (file_exists($pathPNG)) {
+        $imageName = $IDPregunta . ".PNG";
+    }
+
+    if(!is_null($imageName)){
+        setIDinDDBB($imageName);
+    }
+}
+
+function setIDinDDBB($imageName)
+{
+    global $servername, $username, $password, $dbname;
+    //Crear la escritura en base de datos
+    try {
+        $conn2 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')
+        //UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1
+        $sql2 = "UPDATE pregunta SET idImagen = " . $imageName;
+        // use exec() because no results are returned
+        $conn2->exec($sql2);
+        //$response["response"] = 'exito';
+    } catch (PDOException $e) {
+        echo "Error al " . $sql2 . "<br>" . $e->getMessage();
+    }
+
+    $conn2 = null;
+}
--- Servicios/updateFullQuestionInfo.php
+++ Servicios/updateFullQuestionInfo.php
@@ -1,48 +1,40 @@
 <?php
-    //Informacion de la BBDD
-    $servername = "localhost";
-    $username = "u526597556_dev";
-    $password = "1BLeeAgwq1*isgm&jBJe";
-    $dbname = "u526597556_kaanbal";
+require "DDBBVariables.php";
 
-    //Leer las variables del POST
-    $leccion = $_POST['leccion'];
-    $numeroPregunta = $_POST['numeroPregunta'];
-    $pregunta = $_POST['pregunta'];
-    $respuesta_correcta = $_POST['respuesta_correcta'];
-    $respuesta2 = $_POST['respuesta2'];
-    $respuesta3 = $_POST['respuesta3'];
-    $respuesta4 = $_POST['respuesta4'];
-    $question = $_POST['question'];
-    $correct_answer = $_POST['correct_answer'];
-    $answer2 = $_POST['answer2'];
-    $answer3 = $_POST['answer3'];
-    $answer4 = $_POST['answer4'];
-    $tipo = $_POST['tipo'];
+//Leer las variables del POST
+$leccion = $_POST['leccion'];
+$numeroPregunta = $_POST['numeroPregunta'];
+$pregunta = $_POST['pregunta'];
+$respuesta_correcta = $_POST['respuesta_correcta'];
+$respuesta2 = $_POST['respuesta2'];
+$respuesta3 = $_POST['respuesta3'];
+$respuesta4 = $_POST['respuesta4'];
+$question = $_POST['question'];
+$correct_answer = $_POST['correct_answer'];
+$answer2 = $_POST['answer2'];
+$answer3 = $_POST['answer3'];
+$answer4 = $_POST['answer4'];
+$tipo = $_POST['tipo'];
 
-    //Crear la escritura en base de datos
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')
-        //UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1
-        $sql = "UPDATE pregunta SET pregunta = '".$pregunta."', respuesta_correcta = '".$respuesta_correcta."', respuesta2 = '".$respuesta2."', respuesta3 = '".$respuesta3."', respuesta4 = '".$respuesta4."', question = '".$question."', correct_answer = '".$correct_answer."', answer2 = '".$answer2."', answer3 = '".$answer3."', answer4 = '".$answer4."', tipo = '".$tipo."' WHERE id_leccion = '".$leccion."' AND orden = '".$numeroPregunta."'";
-        // use exec() because no results are returned
-        $conn->exec($sql);
-        $response["response"] = 'exito';
-        //$response["response"] = $sql;
-      } catch(PDOException $e) {
-        $response["response"] = $sql . "<br>" . $e->getMessage();
-      }
-      
-      $conn = null;
-    
+//Crear la escritura en base de datos
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')
+  //UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1
+  $sql = "UPDATE pregunta SET pregunta = '" . $pregunta . "', respuesta_correcta = '" . $respuesta_correcta . "', respuesta2 = '" . $respuesta2 . "', respuesta3 = '" . $respuesta3 . "', respuesta4 = '" . $respuesta4 . "', question = '" . $question . "', correct_answer = '" . $correct_answer . "', answer2 = '" . $answer2 . "', answer3 = '" . $answer3 . "', answer4 = '" . $answer4 . "', tipo = '" . $tipo . "' WHERE id_leccion = '" . $leccion . "' AND orden = '" . $numeroPregunta . "'";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  $response["response"] = 'exito';
+  //$response["response"] = $sql;
+} catch (PDOException $e) {
+  $response["response"] = $sql . "<br>" . $e->getMessage();
+}
 
-
-    ////////////////    
-    echo json_encode($response); 
-?>
+$conn = null;
 
 
 
+////////////////    
+echo json_encode($response);
