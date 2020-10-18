dac47ca7b80fc629942529862c74a59a7632b418
Brandon Juárez Ponce <brjupo@gmail.com>
2020-10-18 14:31:04 -0500

    kasdn

--- Front/pagoSeguro/ml.php
+++ Front/pagoSeguro/ml.php
@@ -7,17 +7,18 @@
     <link rel="shortcut icon" type="image/x-icon" href="../CSSsJSs/icons/pyramid.svg" />
     <title>Kaanbal</title>
     <link rel="stylesheet" href="../CSSsJSs/bootstrap441.css" />
+    <link rel="stylesheet" href="ml.css" />
     <script src="../CSSsJSs/minAJAX.js"></script>
 </head>
 
 <body>
 
     <?php
-    
+
     try {
         // SDK de Mercado Pago
-        //require '../../../../../../../vendor/autoload.php';
-        include '../../../../../../vendor/autoload.php';
+        require '../../../../../../vendor/autoload.php';
+        //include '../../../../../../vendor/autoload.php';
         // Agrega credenciales
         MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
 
@@ -34,10 +35,10 @@
     } catch (Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
-    
+
     ?>
 
-<div class="container">
+    <div class="container">
         <div class="row">
             <div class="textCenter col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                 <p><?php echo __DIR__; ?></p>
@@ -60,19 +61,15 @@
         <div class="row">
             <div class="textCenter col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
             <div class="textCenter col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
-                <input type="text" id="token" value="<?php echo $tokenLink ?>" style="display:none;">
 
-                <label for="correo_e">Ingresa tu usuario de staff</label>
-                <input type="text" id="correo_e" name="correo_e">
+                <label for="Concepto">Concepto</label>
+                <input type="text" id="Concepto" name="Concepto" value="Licencia semestral Kaanbal [1 materia]" disabled>
 
-                <label for="psw">Ingresa tu password de staff</label>
-                <input type="text" id="psw" name="psw" />
+                <label for="Cantidad">Cantidad</label>
+                <input type="text" id="Cantidad" name="Cantidad" value="1" disabled/>
 
-                <label for="password2">Ingresa alumno Kaanbal</label>
-                <input type="text" id="psw2" name="psw2" />
-
-                <label for="password2">Correo de alumno a enviar contraseña</label>
-                <input type="text" id="correoAlumno" name="psw2" />
+                <label for="Precio">Precio</label>
+                <input type="text" id="Precio" name="Precio" value="250.00 MXN" disabled/>
                 </form>
             </div>
             <div class="textCenter col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
@@ -84,7 +81,7 @@
             <div class="textCenter col-2 col-sm-2 col-md-3 col-lg-3 col-xl-3"></div>
             <div class="textCenter col-8 col-sm-8 col-md-6 col-lg-6 col-xl-6">
                 <button class="boton2" id="submit">
-                    Submit
+                    Pagar
                 </button>
             </div>
             <div class="textCenter col-2 col-sm-2 col-md-3 col-lg-3 col-xl-3"></div>
