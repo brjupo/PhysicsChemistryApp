87ca854dacc5dc97bcedb0cbfaf989aeffb1a009
Brandon Juárez Ponce <brjupo@gmail.com>
2020-10-31 21:28:10 -0600

    kasndfkj

--- Front/pagoSeguro/mx.php
+++ Front/pagoSeguro/mx.php
@@ -1,28 +1,35 @@
-<?php
-
-$url = 'https://kaanbal.net/DEV/Servicios/getFirstPart.php';
-$data = array('tokenHora' => 'nda0913fTY673o84KJ');
-
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$json = file_get_contents($url, false, $context);
-
-$result = json_decode($json, TRUE);
-
-?>
-
 <html>
-    <body>
-        <p><?php echo $result["response"];?></p>
-        <p>.</p>
-        <p>.</p>
-        <p><?php echo $result["response"];?></p>
-    </body>
+
+<body>
+    <?php
+    $url = 'https://kaanbal.net/DEV/Servicios/getFirstPart.php';
+    $data = array('tokenHora' => 'nda0913fTY673o84KJ');
+
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $json = file_get_contents($url, false, $context);
+
+    $result = json_decode($json, TRUE);
+    ?>
+
+
+    <p><?php echo $url ;?></p>
+    <p>.</p>
+    <p>.</p>
+    <p><?php echo $data ;?></p>
+    <p>.</p>
+    <p>.</p>
+    <p><?php echo $result;?></p>
+    <p>.</p>
+    <p>.</p>
+    <p><?php echo $result["response"]; ?></p>
+</body>
+
 </html>
