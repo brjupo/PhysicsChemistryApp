0907afb5862bc003299c5a75839607aa82c840ff
Brandon Juárez Ponce <brjupo@gmail.com>
2020-11-12 00:01:50 -0600

    kasjdlaksdj

--- servicios/mp_not_webhook_sand.php
+++ servicios/mp_not_webhook_sand.php
@@ -1,9 +1,9 @@
 <?php
 require "DDBBVariables.php";
-
+//Leer el body tipo JSON que trae la consulta de mp
 $entityBody = file_get_contents('php://input');
-//$entityBody = stream_get_contents(STDIN);
 $result = json_decode($entityBody, TRUE);
+
 //Establecer uso horario para el envio de fecha y hora
 function getDatetimeNow()
 {
@@ -12,18 +12,31 @@ function getDatetimeNow()
     $datetime->setTimezone($tz_object);
     return $datetime->format('Y\-m\-d\ H:i:s');
 }
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/*++++++++++++++++++++  VARIABLES PARA EL QUERY  ++++++++++++++++++++*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+$id_mp = $result["date"]["id"];
+//Si llega del boton "probar" de mercado pago, no exite data id, solo id
+if(is_null($id_mp)){
+    $id_mp = $result["id"];
+}
+/* FALTA OBTENER EL CORREO DEL COMPRADOR DE MP ["results"][0]["payer"]["email"] . CON EL CORREO OBTENER EL ID_USUARIO */
+/* FALTA OBTENER EL ["results"][0]["status"]. CON EL STATUS OBTENER EL ID DE STATUS */
 $tiempo = getDatetimeNow();
-//$id_mp = $_POST["id"];
-$body_json = $entityBody;
-$id_mp = $result["id"];
-//Crear la escritura en base de datos
+/* INFO es $entityBody */
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/*++++++++++++++++++++++++  ESCRITURA A BBDD  +++++++++++++++++++++++*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')
     //UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1
-    $sql = "INSERT INTO marketPay (id_market_pay, id_usuario, id_payment_status, tiempo, info) VALUES (" . $id_mp . ", 99999, 1, '" . $tiempo . "', '" . $body_json . "')";
+    $sql = "INSERT INTO marketPay (id_market_pay, id_usuario, id_payment_status, tiempo, info) VALUES (" . $id_mp . ", 99999, 1, '" . $tiempo . "', '" . $entityBody . "')";
     // use exec() because no results are returned
     $conn->exec($sql);
     $response["response"] = 'exito';
