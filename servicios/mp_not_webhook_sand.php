9edf743d59b4d05e4e47b6bf799be452a39f54f2
Brandon Juárez Ponce <brjupo@gmail.com>
2020-11-11 23:30:23 -0600

    aisjdaiosd

--- servicios/mp_not_webhook_sand.php
+++ servicios/mp_not_webhook_sand.php
@@ -23,7 +23,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')
     //UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1
-    $sql = "INSERT INTO marketPay (id_market_pay, id_usuario, id_payment_status, tiempo, info) VALUES (" . $id_mp . ", 99999, 1, '" . $tiempo . "', " . $body_json . ")";
+    $sql = "INSERT INTO marketPay (id_market_pay, id_usuario, id_payment_status, tiempo, info) VALUES (" . $id_mp . ", 99999, 1, '" . $tiempo . "', '" . $body_json . "')";
     // use exec() because no results are returned
     $conn->exec($sql);
     $response["response"] = 'exito';
