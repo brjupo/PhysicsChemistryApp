a94da3ca5d51dd80c3c75beca8500a4bbed895b4
Brandon Juárez Ponce <brjupo@gmail.com>
2020-12-20 15:26:08 -0600

    qwr

---
+++ servicios/Obsoletos/test.php
@@ -0,0 +1,31 @@
+<?php
+require "../01readAndWriteDDBB.php";
+$teacherMail = new queryToDDBB("SELECT mail FROM usuario_prueba WHERE mail = 'jehoyos@tec.mx' ;");
+
+$response["response"] = $teacherMail->read();
+/*
+//Leer las variables del POST
+$teacherMail = $_POST["teacherMail"];
+$lowerTeacherMail = strtolower($teacherMail);
+
+if (strpos($lowerTeacherMail, '@tec.mx') === false) {
+    $response["response"] = "¿Quieres obtener tu acceso a Kaanbal? <a href='https://kaanbal.net/contacto.html'>Contáctanos!</a>. Podemos ofrecer a su institución un periodo de prueba GRATUITO. Recibi  " . $teacherMail;
+} else {
+    $teacherMail = new queryToDDBB("SELECT mail FROM usuario_prueba WHERE mail = '" . $teacherMail . "' ;");
+    if (
+        $teacherMail ==
+        $teacherMail->read()
+    ) {
+        $response["response"] = "El usuario ya existe.";
+    } else if (strpos($teacherMail->read(), "failed") !== false) {
+        $response["response"] = "Ha ocurrido un error inesperado. Por favor intenta más tarde." . $teacherMail->read();
+    } else {
+        //enviar correo
+        $response["response"] = "Te hemos enviado un correo desde <strong>licencias@kaanbal.net</strong> el cual indica el proceso a seguir. Por favor revisa tu carpeta de junk mail, spam o correo no deseado.Recibi  " . $teacherMail . " y la lectura fue: " . $teacherMail->read();
+    }
+}
+*/
+
+////////////////
+header('Content-Type: application/json');
+echo json_encode($response);
