c882fc97600d8cf8433da134ad98573360d8425d
Brandon Juárez Ponce <brjupo@gmail.com>
2020-12-20 15:35:59 -0600

    aksd

--- servicios/Obsoletos/test.php
+++ servicios/Obsoletos/test.php
@@ -1,34 +1,26 @@
 <?php
 require "../01readAndWriteDDBB.php";
-$teacherMail = new queryToDDBB("SELECT mail FROM usuario_prueba WHERE mail = 'jehoyos@tec.mx' ;");
-$getTeacherMail = $teacherMail->read();
-if ($teacherMail == $getTeacherMail) {
-    $response["response"] = "true " . $getTeacherMail;
-} else {
-    $response["response"] = $getTeacherMail;
-}
-/*
+
+
 //Leer las variables del POST
 $teacherMail = $_POST["teacherMail"];
 $lowerTeacherMail = strtolower($teacherMail);
 
 if (strpos($lowerTeacherMail, '@tec.mx') === false) {
-    $response["response"] = "¿Quieres obtener tu acceso a Kaanbal? <a href='https://kaanbal.net/contacto.html'>Contáctanos!</a>. Podemos ofrecer a su institución un periodo de prueba GRATUITO. Recibi  " . $teacherMail;
+    $response["response"] = "¿Quieres obtener tu acceso a Kaanbal? <a href='https://kaanbal.net/contacto.html'>Contáctanos!</a>. Podemos ofrecer a su institución un periodo de prueba GRATUITO.";
 } else {
-    $teacherMail = new queryToDDBB("SELECT mail FROM usuario_prueba WHERE mail = '" . $teacherMail . "' ;");
-    if (
-        $teacherMail ==
-        $teacherMail->read()
-    ) {
+    $getTeacherMail = new queryToDDBB("SELECT mail FROM usuario_prueba WHERE mail = '" . $teacherMail . "' ;");
+    $gettedMail = $getTeacherMail->read();
+    if ($teacherMail == $gettedMail) {
         $response["response"] = "El usuario ya existe.";
-    } else if (strpos($teacherMail->read(), "failed") !== false) {
-        $response["response"] = "Ha ocurrido un error inesperado. Por favor intenta más tarde." . $teacherMail->read();
+    } else if (strpos($gettedMail, "failed") !== false) {
+        $response["response"] = "Ha ocurrido un error inesperado. Por favor intenta más tarde.";
     } else {
         //enviar correo
-        $response["response"] = "Te hemos enviado un correo desde <strong>licencias@kaanbal.net</strong> el cual indica el proceso a seguir. Por favor revisa tu carpeta de junk mail, spam o correo no deseado.Recibi  " . $teacherMail . " y la lectura fue: " . $teacherMail->read();
+        $response["response"] = "Te hemos enviado un correo desde <strong>licencias@kaanbal.net</strong> el cual indica el proceso a seguir. Por favor revisa tu carpeta de junk mail, spam o correo no deseado.";
     }
 }
-*/
+
 
 ////////////////
 header('Content-Type: application/json');
