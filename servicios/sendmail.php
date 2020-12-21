7eb1d250cd253bd604cfa6e4f2e29cde80392f1f
Cristian <cmendez222@gmail.com>
2020-12-21 13:46:33 -0600

    test

--- servicios/sendmail.php
+++ servicios/sendmail.php
@@ -1,7 +1,7 @@
 <?php
 
     use PHPMailer\PHPMailer\PHPMailer;
-    require 'vendor/autoload.php';
+    require '../../vendor/autoload.php';
     $mail = new PHPMailer;
     $mail->isSMTP();
     $mail->SMTPDebug = 2;
