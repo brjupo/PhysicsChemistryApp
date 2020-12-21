3eac6a673d0556f1a3567ae55a04fbb4341e3139
Cristian <cmendez222@gmail.com>
2020-12-21 13:38:16 -0600

    dsdsds

---
+++ servicios/sendmail.php
@@ -0,0 +1,26 @@
+<?php
+
+function sendMail(){
+    use PHPMailer\PHPMailer\PHPMailer;
+    require 'vendor/autoload.php';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 2;
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'licencias@kaanbal.net';
+    $mail->Password = 'BraEduCri567';
+    $mail->setFrom('licencias@kaanbal.net');
+    $mail->addAddress('cmendez222@gmail.com');
+    $mail->Subject = 'Testing PHPMailer';
+    $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+    $mail->Body = 'This is a plain text message body';
+    //$mail->addAttachment('test.txt');
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'The email message was sent.';
+    }
+}
+?>
