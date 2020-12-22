d62abd275ef800ead2c87573d55f65531c3e8661
Brandon Juárez Ponce <brjupo@gmail.com>
2020-12-22 00:05:10 -0600

    aksdlkam

--- Front/Inicio/calificaciones.php
+++ Front/Inicio/calificaciones.php
@@ -13,8 +13,8 @@ if ($_SESSION["mail"] != $_POST["mail"]) {
     header('Location: perfil.php');
     exit;
 }
-
-//$_SESSION["idAsignatura"]
+$mailUsuario = $_SESSION["mail"];
+$idAsigntura = $_SESSION["idAsignatura"]
 
 //ESTO ESTÁ DLV, SOLO APLICA PARA ALUMNOS CON UNA SOLA LICENCIA
 //REPETIRLO SI LLEGA A APLICAR QUE SE TIENE MAS DE UNA LICENCIA
@@ -96,27 +96,76 @@ if ($_SESSION["mail"] != $_POST["mail"]) {
                         <td>Subtema</td>
                         <td>Lección</td>
                         <td>ID Lección</td>
+                        <td>Práctica</td>
+                        <td>Sprint</td>
+                        <td>Examen</td>
                     </tr>
                     <?php
-                    //Crear la lectura en base de datos
+                    //Crear la lectura en base de datos, de temas, subtemas, lecciones y ID Lección
                     try {
                         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
                         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                         $stringQuery = "SELECT t.nombre, s.nombre, l.nombre, l.id_leccion 
                         FROM leccion l JOIN subtema s JOIN tema t 
                         ON l.id_subtema = s.id_subtema AND s.id_tema = t.id_tema 
-                        WHERE t.id_asignatura = 1 
+                        WHERE t.id_asignatura = " . $idAsignatura . " 
                         ORDER BY t.orden, s.orden, l.orden;";
                         $stmt = $conn->query($stringQuery);
                         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                            echo "
-                            <tr>
-                                <td>".$row[0]."</td>
-                                <td>".$row[1]."</td>
-                                <td>".$row[2]."</td>
-                                <td>".$row[3]."</td>
-                            </tr>
-                            ";
+                            echo "<tr>
+                                    <td>" . $row[0] . "</td>
+                                    <td>" . $row[1] . "</td>
+                                    <td>" . $row[2] . "</td>
+                                    <td>" . $row[3] . "</td>";
+                            //Crear la lectura en base de datos, para obtener calificación de práctica
+                            try {
+                                $conn2 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                                $conn2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                $stringQuery2 = "SELECT p.puntuacion 
+                                FROM puntuacion p JOIN usuario_prueba u 
+                                ON p.id_usuario = u.id_usuario 
+                                WHERE p.id_leccion = " . $row[3] . " AND u.mail = " . $mailUsuario . " AND p.tipo = 'PP' ORDER BY p.puntuacion DESC LIMIT 1;";
+                                $stmt2 = $conn2->query($stringQuery2);
+                                while ($row2 = $stmt2->fetch(PDO::FETCH_NUM)) {
+                                    echo "<td>" . $row2[0] . "</td>";
+                                }
+                            } catch (PDOException $e2) {
+                                echo "Error: " . $e2->getMessage();
+                            }
+                            $conn2 = null;
+                            //Crear la lectura en base de datos, para obtener calificación de SPRINT
+                            try {
+                                $conn3 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                                $conn3->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                $stringQuery3 = "SELECT p.puntuacion 
+                                FROM puntuacion p JOIN usuario_prueba u 
+                                ON p.id_usuario = u.id_usuario 
+                                WHERE p.id_leccion = ".$row[3]." AND u.mail = ".$mailUsuario ." AND p.tipo = 'SP' ORDER BY p.puntuacion DESC LIMIT 1;";
+                                $stmt3 = $conn3->query($stringQuery3);
+                                while ($row3 = $stmt3->fetch(PDO::FETCH_NUM)) {
+                                    echo "<td>" . $row3[0] . "</td>";
+                                }
+                            } catch (PDOException $e3) {
+                                echo "Error: " . $e3->getMessage();
+                            }
+                            $conn3 = null;
+                            //Crear la lectura en base de datos, para obtener calificación de EXAMEN
+                            try {
+                                $conn4 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                                $conn4->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                $stringQuery4 = "SELECT p.puntuacion 
+                                FROM puntuacion p JOIN usuario_prueba u 
+                                ON p.id_usuario = u.id_usuario 
+                                WHERE p.id_leccion = ".$row[3]." AND u.mail = ".$mailUsuario ." AND p.tipo = 'E' ORDER BY p.puntuacion DESC LIMIT 1;";
+                                $stmt4 = $conn4->query($stringQuery2);
+                                while ($row4 = $stmt4->fetch(PDO::FETCH_NUM)) {
+                                    echo "<td>" . $row4[0] . "</td>";
+                                }
+                            } catch (PDOException $e4) {
+                                echo "Error: " . $e4->getMessage();
+                            }
+                            $conn4 = null;
+                            echo "</tr>";
                         }
                     } catch (PDOException $e) {
                         echo "Error: " . $e->getMessage();
@@ -127,249 +176,6 @@ if ($_SESSION["mail"] != $_POST["mail"]) {
             </table>
         </div>
     </div>
-
-
-
-
-    <?php
-    //----------------------------------------------------------
-    //----------------------------------------------------------
-    //------------------VERSION OBSOLETA------------------------
-    //----------------------------------------------------------
-    //----------------------------------------------------------
-    ?>
-    <?php
-    //--------------AQUI OBTIENES EL ID DE ||USUARIO|| DEL ALUMNO
-    $alumnos = array();
-    $alumnos["matricula"] = array();
-    $alumnos["id"] = array();
-    array_push($alumnos["matricula"], $mailAlumno);
-    //Crear la lectura en base de datos
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stringQuery = "SELECT id_usuario FROM usuario_prueba WHERE mail = '" . $mailAlumno . "' ";
-        $stmt = $conn->query($stringQuery);
-        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            array_push($alumnos["id"], $row[0]);
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-
-    ?>
-    <?php
-    $id_alumno = $alumnos["id"][0];
-    $id_asignatura = "0";
-    //Crear la lectura en base de datos
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stringQuery = "SELECT id_asignatura FROM licencia WHERE id_usuario = '" . $id_alumno . "' LIMIT 1";
-        $stmt = $conn->query($stringQuery);
-        while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            $id_asignatura = $row[0];
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-    ?>
-    <!--OBTENER LA LISTA DE TEMAS EN ORDEN, DE LA ASIGNATURA-->
-    <div class="container">
-        <div class="row">
-            <?php
-            $temas = array();
-            $temas["nombre"] = array();
-            $temas["id"] = array();
-            //Crear la lectura en base de datos
-            try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stringQuery = "SELECT nombre, id_tema FROM tema WHERE id_asignatura = " . $id_asignatura . " ORDER BY orden";
-                $stmt = $conn->query($stringQuery);
-                while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                    array_push($temas["nombre"], $row[0]);
-                    array_push($temas["id"], $row[1]);
-                }
-            } catch (PDOException $e) {
-                echo "Error: " . $e->getMessage();
-            }
-            $conn = null;
-            ?>
-        </div>
-    </div>
-    <!--OBTENER LA LISTA DE SUBTEMAS EN ORDEN, DE TODOS LOS TEMAS-->
-    <div class="container">
-        <div class="row">
-            <?php
-            $subtemas = array();
-            $subtemas["nombre"] = array();
-            $subtemas["id"] = array();
-            $subtemas["tema"] = array();
-            //Recorreremos todos los temas, y guardaremos en subtemas[nombre] el nombre de TODOS los subtemas por orden de usuario
-            //Eficientizando codigo -> for($i = 0, $size = count($people); $i < $size; ++$i)
-            $sizeTemas = count($temas["id"]);
-            for ($i = 0; $i < $sizeTemas; ++$i) {
-                //Crear la lectura en base de datos
-                try {
-                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $stringQuery = "SELECT nombre, id_subtema FROM subtema WHERE id_tema = " . $temas["id"][$i] . " ORDER BY orden";
-                    $stmt = $conn->query($stringQuery);
-                    while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                        array_push($subtemas["tema"], $temas["nombre"][$i]);
-                        array_push($subtemas["nombre"], $row[0]);
-                        array_push($subtemas["id"], $row[1]);
-                    }
-                } catch (PDOException $e) {
-                    echo "Error: " . $e->getMessage();
-                }
-                $conn = null;
-            }
-
-            ?>
-        </div>
-    </div>
-
-    <!--OBTENER LA LISTA DE LECCIONES EN ORDEN, DE TODOS LOS SUBTEMAS-->
-    <div class="container">
-        <div class="row">
-            <?php
-            $lecciones = array();
-            $lecciones["nombre"] = array();
-            $lecciones["id"] = array();
-            $lecciones["totalPreguntas"] = array();
-            $lecciones["tema"] = array();
-            $lecciones["subtema"] = array();
-            //Recorreremos todos los subtemas, y guardaremos en leccion[nombre] el nombre de TODOS los subtemas por orden de usuario
-            $sizeSubtemas = count($subtemas["id"]);
-            for ($j = 0; $j < $sizeSubtemas; ++$j) {
-                //Crear la lectura en base de datos
-                try {
-                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $stringQuery = "SELECT nombre, id_leccion FROM leccion WHERE id_subtema = " . $subtemas["id"][$j] . " ORDER BY orden";
-                    $stmt = $conn->query($stringQuery);
-                    while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                        array_push($lecciones["tema"], $subtemas["tema"][$j]);
-                        array_push($lecciones["subtema"], $subtemas["nombre"][$j]);
-                        array_push($lecciones["nombre"], $row[0]);
-                        array_push($lecciones["id"], $row[1]);
-                    }
-                } catch (PDOException $e) {
-                    echo "Error: " . $e->getMessage();
-                }
-                $conn = null;
-            }
-            //SELECT COUNT(id_leccion) FROM pregunta WHERE id_leccion = 1 
-            ?>
-        </div>
-    </div>
-
-    <!--IMRPIMIR LA LISTA DE LECCIONES, SUBTEMA Y TEMAS-->
-    <div class="container">
-        <div class="row">
-            <table class="table table-striped">
-                <tbody>
-                    <tr>
-                        <td style="color:rgba(0,0,0,0)">.</td>
-                        <td style="color:rgba(0,0,0,0)">.</td>
-                        <?php
-                        //Recorreremos todos los subtemas, y guardaremos en leccion[nombre] el nombre de TODOS los subtemas por orden de usuario
-                        $sizeLecciones = count($lecciones["id"]);
-                        for ($k = 0; $k < $sizeLecciones; ++$k) {
-                            echo '<td>' . $lecciones["tema"][$k] . '</td>';
-                        } ?>
-                    </tr>
-                    <tr>
-                        <td style="color:rgba(0,0,0,0)">.</td>
-                        <td style="color:rgba(0,0,0,0)">.</td>
-                        <?php
-                        for ($k = 0; $k < $sizeLecciones; ++$k) {
-                            echo '<td>' . $lecciones["subtema"][$k] . '</td>';
-                        }
-                        ?>
-                    </tr>
-                    <tr>
-                        <td style="font-weight:600">Matrícula</td>
-                        <td style="font-weight:600">Modalidad</td>
-                        <?php
-                        //Este for lo aprovecharemos para obtener el total de preguntas de cada leccion
-                        //Ademas de imprimir las lecciones en la tabla
-                        for ($k = 0; $k < $sizeLecciones; ++$k) {
-                            echo '<td>' . $lecciones["nombre"][$k] . '</td>';
-                            //Crear la lectura en base de datos
-                            try {
-                                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                                $stringQuery = "SELECT COUNT(id_leccion) FROM pregunta WHERE id_leccion = " . $lecciones["id"][$k];
-                                $stmt = $conn->query($stringQuery);
-                                while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                                    array_push($lecciones["totalPreguntas"], $row[0]);
-                                }
-                            } catch (PDOException $e) {
-                                echo "Error: " . $e->getMessage();
-                            }
-                            $conn = null;
-                        }
-
-                        ?>
-                    </tr>
-
-                    <?php
-                    //-------------AQUI OBTIENES LA CALIFICACION DEL ALUMNO, SI NO SE ENCUENTRA IMPRIME NP
-                    //Primero crearemos un arreglo con los modos que se quieren E, SP, PP
-                    $modos = array();
-                    $modos["acronimo"] = ["E", "SP", "PP"];
-                    $modos["nombre"] = ["Examen", "Sprint", "Práctica"];
-                    //Despues comenzamos a rotar los alumnos
-                    $sizeAlumnos = count($alumnos["id"]);
-                    $sizeModos = count($modos);
-                    for ($m = 0; $m < $sizeAlumnos; ++$m) {
-                        //Ahora a rotar los modos
-                        for ($p = 0; $p <= $sizeModos; ++$p) {
-                            echo '<tr>';
-                            echo '<td>' . $alumnos["matricula"][$m] . '</td>';
-                            echo '<td>' . $modos["nombre"][$p] . '</td>';
-                            $tipo = $modos["acronimo"][$p];
-                            for ($l = 0; $l < $sizeLecciones; ++$l) {
-                                $entre = 0;
-                                //Crear la lectura en base de datos
-                                try {
-                                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                                    $stringQuery = "SELECT puntuacion FROM puntuacion WHERE tipo ='" . $tipo . "' 
-                                    AND id_leccion=" . $lecciones["id"][$l] . " AND id_usuario =" . $alumnos["id"][$m] . ";";
-                                    $stmt = $conn->query($stringQuery);
-                                    while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                                        $entre = 1;
-                                        $calificacion = intval(100 * $row[0] / $lecciones["totalPreguntas"][$l]);
-                                        if ($tipo == "SP" || $tipo == "SG") {
-                                            ///$calificacion = $calificacion / 3;
-                                            $calificacion = intval(100 * $row[0]  / ($lecciones["totalPreguntas"][$l] * 3));
-                                        }
-                                        echo '<td>' . $calificacion . '</td>';
-                                    }
-                                    if ($entre == 0) {
-                                        echo '<td style="color:red;">NP</td>';
-                                    }
-                                } catch (PDOException $e) {
-                                    echo "Error: " . $e->getMessage();
-                                }
-                                $conn = null;
-                            }
-                            echo '</tr>';
-                        }
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-
 
 </body>
 
