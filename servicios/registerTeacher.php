459af58f10010e0db53f6d3bd9fe566926b47ba3
Brandon Juárez Ponce <brjupo@gmail.com>
2020-12-21 23:09:43 -0600

    aksndjaksnd

--- servicios/registerTeacher.php
+++ servicios/registerTeacher.php
@@ -1,4 +1,5 @@
 <?php
+require "00DDBBVariables.php";
 require "01readAndWriteDDBB.php";
 require "02sendMail.php";
 
@@ -11,49 +12,47 @@ $lowerTeacherMail = strtolower($teacherMail);
 //if (strpos($variable, 'palabra') !== false)
 
 
-$response["response"] = "No quedamos en la línea 14a";
 //if (strpos($lowerTeacherMail, '@itesm.mx') === false) {
 if (false) {
     $response["response"] = "¿Quieres obtener tu acceso a Kaanbal? <a href='https://kaanbal.net/contacto.html'>Contáctanos!</a>. Podemos ofrecer a su institución un periodo de prueba GRATUITO.";
 } else {
     $getTeacherMail = new queryToDDBB("SELECT mail FROM usuario_prueba WHERE mail = '" . $lowerTeacherMail . "' ;");
     $gettedMail = $getTeacherMail->read();
-    $response["response"] = "No quedamos en la línea 20";
     if ($lowerTeacherMail == $gettedMail) {
         $response["response"] = "El usuario ya existe.";
     } else if (strpos($gettedMail, "failed") !== false) {
         $response["response"] = "Ha ocurrido un error inesperado. Por favor intenta más tarde.";
     } else {
-        $response["response"] = "No quedamos en la línea 26";
         //enviar correo
         //function enviarMail($destinatario, $asunto, $cuerpo)
         $respuestaAlEnviarElMail =  enviarMail($lowerTeacherMail, "Registro profesor. Kaanbal", cuerpoCorreoNuevoProfesor());
         if (strpos($respuestaAlEnviarElMail, "failed") !== false) {
             $response["response"] = "Ha ocurrido un error al enviar el correo. Detalle: " . $respuestaAlEnviarElMail;
         } else {
-            $response["response"] = "No quedamos en la línea 33";
             //agregarProfesor a usuario_prueba con password correoCorreo
             $addTeacher = new queryToDDBB("INSERT INTO usuario_prueba (mail, pswd) VALUES ('" . $lowerTeacherMail . "', '" . $lowerTeacherMail . $lowerTeacherMail . "');");
             if ($addTeacher->write() != "success") {
                 $response["response"] = "Error al escribir el nuevo usuario";
             } else {
-                $response["response"] = "No quedamos en la línea 39";
                 //obtener el ID del usuario
                 $getTeacherID = new queryToDDBB("SELECT id_usuario FROM usuario_prueba WHERE mail= '" . $lowerTeacherMail . "';");
                 $gettedTeacherID = $getTeacherID->read();
                 if (!is_numeric($gettedTeacherID)) {
                     $response["response"] = "Error en el ID del nuevo usuario.";
                 } else {
-                    $response["response"] = "No quedamos en la línea 47";
                     //agregar ID profesor a profesor
-                    $addTeacherInTeacher = new queryToDDBB("INSERT INTO profesor (id_usuario) VALUES (" . intval($gettedTeacherID) . ") ;");
-                    $addedTeacherInTeacher = $addTeacherInTeacher->write();
+                    //$addTeacherInTeacher = new queryToDDBB("INSERT INTO profesor (id_usuario) VALUES (" . intval($gettedTeacherID) . ") ;");
+                    //$addedTeacherInTeacher = $addTeacherInTeacher->write();
                     //if ($addedTeacherInTeacher != "success") {
-                    if(true){
-                        $response["response"] = "Error al escribir el profesor";
-                    } else {
+                    try {
+                        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $conn->exec("INSERT INTO profesor (id_usuario) VALUES (" . intval($gettedTeacherID) . ") ;");
                         $response["response"] = "Te hemos enviado un correo desde <strong>licencias@kaanbal.net</strong> el cual indica el proceso a seguir. Por favor revisa tu carpeta de junk mail, spam o correo no deseado.";
+                    } catch (PDOException $e) {
+                        $response["response"] = "Error al escribir el profesor.". "failed:" . "INSERT INTO profesor (id_usuario) VALUES (" . intval($gettedTeacherID) . ") ;". "<br>" . $e->getMessage();;
                     }
+                    $conn = null;
                 }
             }
         }
