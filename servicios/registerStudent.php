00f82a4f89e7ff0ca91d464db6cce98554481a21
Cristian <cmendez222@gmail.com>
2021-01-02 11:51:55 -0600

    huelos

--- servicios/registerStudent.php
+++ servicios/registerStudent.php
@@ -23,6 +23,7 @@ if (false) {
   //Validar que exista GRUPO
   $getGrupoCode = new queryToDDBB("SELECT id_grupo FROM grupo WHERE codigo = '" . $studentCode . "' ;");
   $gettedGroup = $getGrupoCode->read();
+  $gettedGroup = "hola";
   if($gettedGroup != ""){
         //Validar que no exusta Mail
         if ($lowerStudentMail == $gettedMail) {
