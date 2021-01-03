e9f51ea7fc06e0a0753b48dba93dd9398329a58b
Brandon Juárez Ponce <brjupo@gmail.com>
2021-01-02 19:19:52 -0600

    corrigiendo CSS

--- Front/CSSsJSs/mainCSSsJSs.php
+++ Front/CSSsJSs/mainCSSsJSs.php
@@ -2,7 +2,7 @@
 //$variableDeclaradaEnLosArchivos = "nombreMasRecienteDelArchivo.Extension";
 $bootstrap341="bootstrap341.css";
 $bootstrap441="bootstrap441.css";
-$kaanbalEssentials="kaanbalEssentialsDic2020.css";
+$kaanbalEssentials="kaanbalEssentials01.css";
 $minAJAX="minAJAX.js";
 
 
