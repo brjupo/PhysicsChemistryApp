a47cc3a53c6f60479ce64f08efcfd3558c2df6de
Brandon Juárez Ponce <brjupo@gmail.com>
2021-04-04 15:01:23 -0500

    asdn

---
+++ servicios/ZZZ_LecturasSeguridadBBDD.php
@@ -0,0 +1,6 @@
+<?php 
+/*
+https://www.acunetix.com/blog/articles/prevent-sql-injection-vulnerabilities-in-php-applications/
+https://stackoverflow.com/questions/12344741/binding-multiple-values-in-pdo 
+*/
+?>
