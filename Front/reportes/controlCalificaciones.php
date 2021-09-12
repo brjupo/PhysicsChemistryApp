f0599a86a4b8752b4913a9c111494043e3aeae95
Brandon Juárez Ponce <brjupo@gmail.com>
2021-09-11 21:20:00 -0500

    Mejoras reportes

--- Front/reportes/controlCalificaciones.php
+++ Front/reportes/controlCalificaciones.php
@@ -20,16 +20,13 @@ require "../CSSsJSs/mainCSSsJSs.php";
   <!--KAANBAL TITULO-->
   <div class="container">
     <div class="row">
-      <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-      <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <h1 class="titulo">Kaanbal</h1>
       </div>
-      <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
-      <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -45,9 +42,27 @@ require "../CSSsJSs/mainCSSsJSs.php";
         <h4>Reportes</h4>
       </div>
     </div>
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p><strong>RECUERDA:</strong></p>
+        <p>Kaanbal trabaja con el siguiente árbol:</p>
+        <p>- Cada MATERIA tiene varios TEMAS</p>
+        <p>- Cada TEMA tiene varios SUBTEMAS</p>
+        <p>- Cada SUBTEMA tiene varias LECCIONES</p>
+        <p>.</p>
+        <p>Kaanbal tiene 3 modos de juego:</p>
+        <p>Práctica: Se muestran las preguntas en un orden que permiten al alumno aprender de manera escalonada, paso a paso.</p>
+        <p>Sprint: Son las mismas preguntas que la modalidad práctica, pero ordenadas de manera aleatoria y con un límite de tiempo para cada pregunta. Si se equivoca en 1, o el tiempo vence, ese juego termina y muestra su calificación obtenida.</p>
+        <p>Examen: Son las mismas preguntas que la modalidad práctica, pero ordenadas de manera aleatoria y con un límite de tiempo para TODO el juego. Si el tiempo vence, ese juego termina y muestra su calificación obtenida.</p>
+        <p>.</p>
+        <p>Kaanbal trabaja con un sistema de diamantes. </p>
+        <p>Para el caso de la modalidad práctica y examen se otorga 1 diamante por cada respuesta contestada de manera correcta. Si el alumno repite el juego NO obtendrá diamantes adicionales, a menos que, supere su calificación anterior.</p>
+        <p>Para el caso de sprint, se otorgan 3 diamantes si el alumno contestó en la primera tercera parte del tiempo, se otorgan 2 diamantes si el alumno contestó en la segunda tercera parte del tiempo, se otorga 1 diamante si el alumno contestó en la última tercera parte del tiempo. Si el alumno repite el juego NO obtendrá diamantes adicionales, a menos que, supere su calificación anterior.</p>
+      </div>
+    </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -56,7 +71,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -65,7 +80,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -79,20 +94,20 @@ require "../CSSsJSs/mainCSSsJSs.php";
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p>El reporte más completo que te incluye TODAS las calificaciones todas las lecciones, de las tres modalidades [práctica, sprint, examen], de todos los alumnos de UN grupo.</p>
-        <p>Este reporte te será muy útil al cierre del semestre, ya que contiene TODA la información necesaria.</p>
+        <p>El más completo, incluye TODAS las calificaciones todas las lecciones, de las tres modalidades [práctica, sprint, examen], de todos los alumnos de UN grupo.</p>
+        <p>Te será muy útil al cierre del semestre, ya que contiene TODA la información necesaria.</p>
       </div>
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <a href="alumno/elegirGrupo.php">
           <button type="button" class="btn btn-warning">
-            Reporte detalle alumno
+            Detalle alumno
           </button>
         </a>
       </div>
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -101,7 +116,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -114,8 +129,8 @@ require "../CSSsJSs/mainCSSsJSs.php";
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p>Este reporte SOLO incluye las calificaciones de UN tema [incluyendo todos los subtemas y todas las lecciones] y SOLO UNA modalidad [práctica, sprint o examen]</p>
-        <p>Este reporte te será muy útil para subir calificaciones cada que acabes de ver un tema.</p>
+        <p>SOLO incluye las calificaciones de UN tema [incluyendo todos los subtemas y todas las lecciones] y SOLO UNA modalidad [práctica, sprint o examen]</p>
+        <p>Te será muy útil para subir calificaciones cada que acabes de ver un tema.</p>
       </div>
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <a href="todo/elegirGrupo.php">
@@ -127,7 +142,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -136,7 +151,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -149,8 +164,8 @@ require "../CSSsJSs/mainCSSsJSs.php";
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p>Este reporte SOLO incluye la suma de los diamantes obtenidos en un periodo de tiempo.</p>
-        <p>Este reporte te será muy útil para fijar a tus alumnos una meta y valides que han usado la aplicación web.</p>
+        <p>SOLO incluye la suma de los diamantes obtenidos en un periodo de tiempo.</p>
+        <p>Te será muy útil para fijar a tus alumnos una meta y valides que han usado la aplicación web.</p>
         <p style="font-size:small">El tiempo se determina al momento que el alumno FINALIZA su ejercicio.</p>
       </div>
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -163,7 +178,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -172,7 +187,7 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -185,9 +200,9 @@ require "../CSSsJSs/mainCSSsJSs.php";
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p>Este reporte SOLO incluye las calificaciones que el alumno obtuvo en un periodo de tiempo específico. UNA lección, UNA modalidad.</p>
-        <p>Este reporte te será muy útil a la hora de hacer algún examen en clase o ejercicio en clase</p>
-        <p style="font-size:small">Imprime la calificación MÁS alta del alumno obtenido UNICAMENTE en ese lapso de tiempo. El tiempo se determina al momento que el alumno FINALIZA su ejercicio.</p>
+        <p>SOLO incluye las calificaciones que el alumno obtuvo en un periodo de tiempo específico. UNA lección, UNA modalidad.</p>
+        <p>Te será muy útil a la hora de hacer algún examen en clase o ejercicio en clase</p>
+        <p style="font-size:small">Imprime la calificación MÁS alta del alumno obtenido ÚNICAMENTE en ese lapso de tiempo. El tiempo se determina al momento que el alumno FINALIZA su ejercicio.</p>
       </div>
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <a href="elegirTSL/elegirAsignatura.php">
@@ -199,7 +214,16 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -212,8 +236,8 @@ require "../CSSsJSs/mainCSSsJSs.php";
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <p>Este reporte SOLO incluye el tiempo en minutos que el alumno ha jugado en Kaanbal</p>
-        <p>Este reporte te será muy útil para controlar el uso de dispositivos electrónicos de tus alumnos</p>
+        <p>SOLO incluye el tiempo en minutos que el alumno ha jugado en Kaanbal</p>
+        <p>Te será muy útil para controlar el uso de dispositivos electrónicos de tus alumnos</p>
       </div>
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <a href="tiempo/elegirGrupo.php">
@@ -225,7 +249,16 @@ require "../CSSsJSs/mainCSSsJSs.php";
     </div>
   </div>
 
-  <!--ESPACIO VACIO-->
+  <!-- -->
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p style="color: rgba(0, 0, 0, 0);">.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- -->
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
