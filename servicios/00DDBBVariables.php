e79ed5ef4499fbd72c3b87273b4f9e91a6759d52
Brandon Juárez Ponce <brjupo@gmail.com>
2022-08-19 00:40:54 -0500

    editando

--- servicios/00DDBBVariables.php
+++ servicios/00DDBBVariables.php
@@ -3,6 +3,6 @@
 //Si lo borran, esas vistas no funcionaran o estaran llenas de errores
 //Informacion de la BBDD
 $servername = "localhost";
-$username = "u526597556_dev";
-$password = "1BLeeAgwq1*isgm&jBJe";
-$dbname = "u526597556_kaanbal";
+$username = "";
+$password = "";
+$dbname = "";
